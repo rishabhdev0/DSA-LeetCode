d00d9f97cbc278481bfd63773710d9e87d00c45b
rishabhdev0 <rishabh52003@gmail.com>
2025-01-08 17:23:21 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (23.38%) - LeetSync

---
+++ 66-plus-one/plus-one.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+    int n=digits.size();
+     for(int i=n-1;i>=0;--i){
+        if(digits[i]<9){
+            digits[i]++;
+            return digits;
+        }
+
+        // if the last element is 0 then we check and insert the +1 in the front for example 999 = last digit is 9 +1=0 then in the front we will insert 1 it will become 1000
+         digits[i]=0;
+     }    
+     digits.insert(digits.begin(),1);
+
+     return digits;
+    }
+
+};
