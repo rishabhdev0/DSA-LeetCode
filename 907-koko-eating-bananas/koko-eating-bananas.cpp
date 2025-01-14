98e48040a3c2153b3637640d5b262bc672c53499
rishabhdev0 <rishabh52003@gmail.com>
2025-01-14 18:32:54 +0530

    Time: 8 ms (63.26%) | Memory: 24.1 MB (8.27%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,5 +1,16 @@
 class Solution {
 public:
+int find_max(vector<int>piles){
+    int maxi=INT_MIN;
+    int n=piles.size();
+    for(int i=0;i<n;i++){
+        maxi=max(piles[i],maxi);
+    }
+    return maxi;
+}
+
+
+
     int check(vector<int>& piles, int h, int mid) {
         int ans = 0;
         for (int i = 0; i < piles.size(); i++) {
@@ -11,10 +22,11 @@ public:
         return ans <= h;
     }
 
+
     int minEatingSpeed(vector<int>& piles, int h) {
         int n = piles.size();
         int low = 1;
-        int high = *max_element(piles.begin(), piles.end());
+        int high = find_max(piles);
 
         while (low < high) {
             int mid = (low + high) >> 1;
