410d6aa680ab94c785974c939cb2df84681a8bb7
rishabhdev0 <rishabh52003@gmail.com>
2025-01-22 16:08:16 +0530

    Time: 3 ms (57.50%) | Memory: 18.2 MB (29.36%) - LeetSync

---
+++ 39-combination-sum/combination-sum.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    void recursion(vector<int>& candidates, int target, int index, vector<vector<int>>& ans, vector<int>& output) {
+        if (index >= candidates.size()) {
+             if (target == 0) {
+            ans.push_back(output);  
+            return;
+        }
+            return;
+        }
+
+        recursion(candidates, target, index + 1, ans, output);
+
+        if (candidates[index] <= target) {
+            output.push_back(candidates[index]);  
+            recursion(candidates, target - candidates[index], index, ans, output);  // 
+            output.pop_back();  
+        }
+    }
+
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        vector<int> output;
+        int index = 0;
+        recursion(candidates, target, index, ans, output);  // Start recursion
+        return ans;
+    }
+};
