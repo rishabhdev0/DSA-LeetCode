d869f9bdb329b6aa81295cf93245f64de69bf152
rishabhdev0 <rishabh52003@gmail.com>
2025-05-14 18:27:55 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (96.13%) - LeetSync

--- 768-partition-labels/partition-labels.cpp
+++ 768-partition-labels/partition-labels.cpp
@@ -4,14 +4,14 @@ public:
         int n = s.length();
         vector<int>result;
         vector<int> freq(26 , -1);
-        // last occurances if the charcter
+       // last index
         for(int i = 0 ; i<n;i++){
            int idx = s[i]-'a';
             freq[idx]=i;
         }
        int i = 0;
         while(i<n){
-            int end = freq[s[i]-'a']; // last occurances
+            int end = freq[s[i]-'a'];
             int j = i;
             while(j<end){
                 end = max(end , freq[s[j]-'a']);
