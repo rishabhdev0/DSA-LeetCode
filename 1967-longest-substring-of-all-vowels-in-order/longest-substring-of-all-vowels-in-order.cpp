0fcd05a67b9a282601403377136049819cc19789
rishabhdev0 <rishabh52003@gmail.com>
2025-05-20 23:36:20 +0530

    Time: 9 ms (74.32%) | Memory: 29.5 MB (43.24%) - LeetSync

--- 1967-longest-substring-of-all-vowels-in-order/longest-substring-of-all-vowels-in-order.cpp
+++ 1967-longest-substring-of-all-vowels-in-order/longest-substring-of-all-vowels-in-order.cpp
@@ -1,30 +1,39 @@
 class Solution {
 public:
     int longestBeautifulSubstring(string word) {
+        string vowels = "aeiou";
         int n = word.length();
         int max_len = 0;
-        int count = 1;  // number of vowels in order
-        int len = 1;
+        int i = 0;
 
-        for (int i = 1; i < n; ++i) {
-            if (word[i] >= word[i-1]) { // prev elem same or smaller
-                len++;
-                if (word[i] != word[i-1]) { // uniqu vovwel count 
-                    count++;
-                }
-                if (count == 5) { // all 5 vowel find
-                    max_len = max(max_len, len);
-                }
-            } else {
-               // of order break then we start over
-                if (word[i] == 'a') {
-                    len = 1;
-                    count = 1;
-                } else {
-                    len = 0;
-                    count = 0;
-                }
+        while (i < n) {
+            // find until we get a 
+            if (word[i] != 'a') {
+                i++;
+                continue;
             }
+
+            int j = i; // starting of slifding window
+            int vowel_index = 0; // keep track to vowel index for the correct order
+
+            while (j < n && word[j] >= vowels[vowel_index]) {
+                if (word[j] == vowels[vowel_index]) { // 
+                //    continue;
+                } else if (vowel_index < 4 && word[j] == vowels[vowel_index + 1]) {
+                    vowel_index++; // move to next vowel
+                } else {
+                    break; // subsstring broken
+                }
+                j++;
+            }
+
+            // Check if all 5 vowels were found
+            if (vowel_index == 4) {
+                max_len = max(max_len, j - i);
+            }
+
+            // move to where the substring got broken
+            i = j;
         }
 
         return max_len;
