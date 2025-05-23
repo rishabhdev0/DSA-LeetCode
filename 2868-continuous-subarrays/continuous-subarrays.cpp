3e477f8839478120344061373e3658d31f693ed2
rishabhdev0 <rishabh52003@gmail.com>
2025-05-23 19:42:00 +0530

    Time: 81 ms (89.54%) | Memory: 113 MB (82.82%) - LeetSync

--- 2868-continuous-subarrays/continuous-subarrays.cpp
+++ 2868-continuous-subarrays/continuous-subarrays.cpp
@@ -2,25 +2,22 @@ class Solution {
 public:
     long long continuousSubarrays(vector<int>& nums) {
         int n = nums.size();
-        map<int, int> mp;
-        int i = 0;
+        map<int,int> freq;
+        int left = 0, right = 0;
+
         long long count = 0;
-        
-        for (int j = 0; j < n; j++) {
-            mp[nums[j]]++;
-            
-            // Calculate current max - min
-            while (mp.rbegin()->first - mp.begin()->first > 2) {
-                mp[nums[i]]--; // we will remove the ith element and now shrink
-                if (mp[nums[i]] == 0) {
-                    mp.erase(nums[i]);
+        while(right < n){
+            freq[nums[right]]++;
+            while(freq.rbegin()->first - freq.begin()->first > 2){
+                freq[nums[left]]--;
+                if(freq[nums[left]] == 0){
+                    freq.erase(nums[left]);
                 }
-                i++;
+                left++;
             }
-            
-            count += j - i + 1;
+            count += right-left+1;
+            right++;
         }
-        
         return count;
     }
 };
