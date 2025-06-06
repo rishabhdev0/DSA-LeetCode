1a4eba9916d93c9afe88cf5cbae2e22d84a92990
rishabhdev0 <rishabh52003@gmail.com>
2025-06-06 11:43:05 +0530

    Time: 11 ms (69.14%) | Memory: 35 MB (66.71%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -1,51 +1,35 @@
 class Solution {
 private:
-    int find_max(const vector<int>& weights) {
-        int maxi = INT_MIN;
-        for (int weight : weights) {
-            maxi = max(weight, maxi);
-        }
-        return maxi;
-    }
-
-    int find_sum(const vector<int>& weights) {
-        int totalweight = 0;
-        for (int weight : weights) {
-            totalweight += weight;
-        }
-        return totalweight;
-    }
-
-    bool canShip(const vector<int>& weights, int capacity, int days) {
+    bool can_ship(vector<int>& weights, int days, int capacity) {
         int load = 0;
-        int dayCount = 1;
+        int day_count = 1;
+
         for (int weight : weights) {
             if (load + weight > capacity) {
-                dayCount++;
-                load = weight;
-                if (dayCount > days) {
-                    return false;
-                }
+                day_count++;      
+                load = weight;    
             } else {
                 load += weight;
             }
         }
-        return true;
+
+        return day_count <= days; 
     }
 
 public:
     int shipWithinDays(vector<int>& weights, int days) {
-        int left = find_max(weights);
-        int right = find_sum(weights);
+        int left = *max_element(weights.begin(), weights.end());
+        int right = accumulate(weights.begin(), weights.end(), 0);
         int result = right;
 
         while (left <= right) {
             int mid = left + (right - left) / 2;
-            if (canShip(weights, mid, days)) {
+
+            if (can_ship(weights, days, mid)) {
                 result = mid;
-                right = mid - 1;
+                right = mid - 1;  
             } else {
-                left = mid + 1;
+                left = mid + 1;  
             }
         }
 
