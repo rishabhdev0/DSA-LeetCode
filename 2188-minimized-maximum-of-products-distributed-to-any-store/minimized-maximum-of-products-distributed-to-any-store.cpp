1747890e9211be331c3a1733de7eb62f5c503d61
rishabhdev0 <rishabh52003@gmail.com>
2025-06-08 11:34:41 +0530

    Time: 28 ms (71.00%) | Memory: 88.3 MB (75.85%) - LeetSync

---
+++ 2188-minimized-maximum-of-products-distributed-to-any-store/minimized-maximum-of-products-distributed-to-any-store.cpp
@@ -0,0 +1,29 @@
+class Solution {
+private:
+    bool canDistribued(vector<int>& quantities , int n , int mid){
+        int stores_needed = 0;
+        for(int quantity : quantities){
+            stores_needed += (quantity + mid - 1) / mid;  
+        }
+        return stores_needed <= n;
+    }
+
+public:
+    int minimizedMaximum(int n, vector<int>& quantities) {
+        int left = 1;
+        int right = *max_element(quantities.begin(), quantities.end());
+        int result = right;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (canDistribued(quantities, n, mid)) {
+                result = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return result;
+    }
+};
