536d3f76daea95e4a9c3839ef256a1d62296bd02
rishabhdev0 <rishabh52003@gmail.com>
2025-06-14 11:14:58 +0530

    Time: 4 ms (34.00%) | Memory: 31.8 MB (27.89%) - LeetSync

--- 581-shortest-unsorted-continuous-subarray/shortest-unsorted-continuous-subarray.cpp
+++ 581-shortest-unsorted-continuous-subarray/shortest-unsorted-continuous-subarray.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     int findUnsortedSubarray(vector<int>& nums) {
         int n = nums.size();
-        vector<int>sorted(nums);
-        sort(sorted.begin(),sorted.end());
-        if(sorted == nums) return 0;
-        int i = 0 ;
+        vector<int>original(nums);
+        sort(nums.begin(),nums.end());
+        if(original==nums) return 0;
+        int i = 0;
         int j = n-1;
-            while(i<n && sorted[i]==nums[i]){
-                i++;
-            }
-            while(j>=0 && sorted[j]==nums[j]){
-                j--;
-            }
+        while(i<n && original[i]==nums[i]){
+            i++;
+        }
+        while(j>=0 && original[j]==nums[j]){
+            j--;
+        }
         return j-i+1;
     }
 };
