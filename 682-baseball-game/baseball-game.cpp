060f6b0161efe579c6b984eca61446dc272fae03
rishabhdev0 <rishabh52003@gmail.com>
2025-06-17 22:13:18 +0530

    Time: 4 ms (7.91%) | Memory: 13.9 MB (16.10%) - LeetSync

--- 682-baseball-game/baseball-game.cpp
+++ 682-baseball-game/baseball-game.cpp
@@ -1,19 +1,17 @@
 class Solution {
 private:
-    void performOperation(stack<int>& st, const string& op) {
+    void performoperation(stack<int>& st, const string& op) {
         if (op == "+") {
-            int a = st.top(); st.pop();
+            int a = st.top();
+             st.pop();
             int b = st.top();
-            st.push(a);
+            st.push(a);         
             st.push(a + b);
-        }
-        else if (op == "D") {
-            st.push(2 * st.top());
-        }
-        else if (op == "C") {
+        } else if (op == "D") {
+            st.push(st.top() * 2);
+        } else if (op == "C") {
             st.pop();
-        }
-        else {
+        } else {
             st.push(stoi(op));
         }
     }
@@ -21,17 +19,15 @@ private:
 public:
     int calPoints(vector<string>& operations) {
         stack<int> st;
-        
         for (const string& op : operations) {
-            performOperation(st, op);
+            performoperation(st, op);
         }
-        
+
         int sum = 0;
         while (!st.empty()) {
             sum += st.top();
             st.pop();
         }
-        
         return sum;
     }
 };
