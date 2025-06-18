6804b0224f7f6e55a6f6f4335508824aa7aa4e62
rishabhdev0 <rishabh52003@gmail.com>
2025-06-18 14:53:06 +0530

    Time: 0 ms (100.00%) | Memory: 21.3 MB (99.77%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.cpp
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -8,14 +8,14 @@ public:
             while(!st.empty() && st.back() > 0 && a < 0) {
                 int sum = st.back() + a;
                 
-                if(sum > 0) {       // Top wins
-                    a=0;        
+                if(sum > 0) {       // st.top() wins 
+                    a=0;            // no need to push
                 } 
                 else if(sum < 0) {  // Current wins
-                    st.pop_back();  
+                    st.pop_back();  // pop the element
                 } 
                 else {              // Both destroyed
-                    st.pop_back();
+                    st.pop_back();  // no need to  push and pop the element
                     a=0;
                 }
             }
