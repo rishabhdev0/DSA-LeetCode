818d59974171a3ed3e6baa8b2702bdaae07725ea
rishabhdev0 <rishabh52003@gmail.com>
2025-06-19 11:47:45 +0530

    Time: 0 ms (100.00%) | Memory: 21.9 MB (5.42%) - LeetSync

--- 112-path-sum/path-sum.cpp
+++ 112-path-sum/path-sum.cpp
@@ -9,27 +9,52 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// private:
+//     bool inOrder(TreeNode* root, int& targetSum, int sum) {
+//         if (root == nullptr) return false;
+        
+//         sum += root->val;
+        
+//         // if we reached the  leaf node
+//         if (root->left == nullptr && root->right == nullptr) {
+//             return sum == targetSum;
+//         }
+
+//         bool left = inOrder(root->left, targetSum, sum);
+//         bool right = inOrder(root->right, targetSum, sum);
+        
+//         return left || right;  
+//     }
+
+// public:
+//     bool hasPathSum(TreeNode* root, int targetSum) {
+//         int sum = 0;
+//         return inOrder(root, targetSum, sum);
+//     }
+// };
+
 class Solution {
-private:
-    bool inOrder(TreeNode* root, int& targetSum, int sum) {
-        if (root == nullptr) return false;
-        
-        sum += root->val;
-        
-        // if we reached the  leaf node
-        if (root->left == nullptr && root->right == nullptr) {
-            return sum == targetSum;
-        }
-
-        bool left = inOrder(root->left, targetSum, sum);
-        bool right = inOrder(root->right, targetSum, sum);
-        
-        return left || right;  
-    }
-
 public:
     bool hasPathSum(TreeNode* root, int targetSum) {
-        int sum = 0;
-        return inOrder(root, targetSum, sum);
+       if(root==nullptr) return false;
+       queue<pair<TreeNode* , int>>que;
+       que.push({root, root->val});
+       while(!que.empty()){
+        auto[node , currsum] = que.front();
+        que.pop();
+        // check if its leaf node
+        if(node->left==nullptr &&node->right == nullptr){
+            if(currsum == targetSum) return true;
+        }
+        if(node->left != nullptr){
+            que.push({node->left , currsum + node->left->val});
+        }
+        if(node->right != nullptr){
+            que.push({node->right , currsum + node->right->val});
+        }
+    }
+return false;
     }
 };
+
