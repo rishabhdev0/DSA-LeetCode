a88e0acd70f5561d682bdd4843df7a5bf2ab8214
rishabhdev0 <rishabh52003@gmail.com>
2025-06-24 11:21:06 +0530

    Time: 3 ms (44.14%) | Memory: 12.8 MB (37.29%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,27 +1,21 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> result(nums1.size(), -1);
-
-        for (int i = 0; i < nums1.size(); ++i) {
-            // find index in nums2
-            int indexInNums2 = -1;
-            for (int j = 0; j < nums2.size(); ++j) {
-                if (nums2[j] == nums1[i]) {
-                    indexInNums2 = j;
-                    break;
-                }
+        vector<int>result;
+        unordered_map<int , int> nextGreater;
+        stack<int>st;
+        for(int num : nums2){
+            while(!st.empty() && num > st.top()){
+                nextGreater[st.top()] = num;
+                st.pop();
             }
-
-           // now check for greater element
-            for (int j = indexInNums2 + 1; j < nums2.size(); ++j) {
-                if (nums2[j] > nums1[i]) {
-                    result[i] = nums2[j];
-                    break;
-                }
-            }
+            st.push(num);
         }
+       
+       for(int num : nums1){
+        result.push_back(nextGreater.count(num) ? nextGreater[num] : -1);
+       }
+       return result;
 
-        return result;
     }
 };
