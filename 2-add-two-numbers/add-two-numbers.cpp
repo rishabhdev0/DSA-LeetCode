d7e47ca3a2ac0c4ed63ef377ad915041fa4a31aa
rishabhdev0 <rishabh52003@gmail.com>
2025-07-09 20:25:55 +0530

    Time: 0 ms (100.00%) | Memory: 77.2 MB (43.06%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.cpp
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -1,27 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* dummynode = new ListNode(0);
-        ListNode* current = dummynode;
-        int carry = 0;
-        
-        while(l1 != nullptr || l2 != nullptr || carry != 0) {
-              // checking the l1 or l2 is null or  empty then we took  zero ,otherwise  
-            //  we  took the next element 
-            int x = (l1 != nullptr) ? l1->val : 0;
-            int y = (l2 != nullptr) ? l2->val : 0;
-            int sum = x + y + carry;
-            carry = sum / 10;
-            // creating node for storing ther digit 
-            current->next = new ListNode(sum % 10);
-            current = current->next;
-            
-            if(l1 != nullptr) l1 = l1->next;
-            if(l2 != nullptr) l2 = l2->next;
+     ListNode* dummy_node = new ListNode(-1);
+     ListNode* curr = dummy_node;
+     int carry = 0;
+     while(l1 != nullptr || l2 != nullptr || carry != 0){
+        int sum = carry;
+        if(l1 != nullptr){
+            sum+=l1->val;
+            l1 = l1->next;
         }
-          //    answer starting from next of dummy node 
-        ListNode* ans = dummynode->next;
-        delete dummynode;
-        return ans;
+        if(l2 != nullptr){
+            sum+=l2->val;
+            l2 = l2->next;
+        }
+         carry = sum / 10;
+         curr->next = new ListNode(sum % 10);
+         curr = curr->next;
+     }
+     return dummy_node->next;
     }
 };
