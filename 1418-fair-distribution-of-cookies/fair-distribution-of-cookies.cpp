7c2f8e7eb66120c4abd6ae1583a1bf2169011328
rishabhdev0 <rishabh52003@gmail.com>
2025-07-23 12:02:01 +0530

    Time: 740 ms (43.00%) | Memory: 9.1 MB (20.93%) - LeetSync

---
+++ 1418-fair-distribution-of-cookies/fair-distribution-of-cookies.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int result = INT_MAX;
+    int n;
+
+    void backtrack(vector<int>& cookies, vector<int>& children, int k, int idx) {
+        if (idx >= n) {
+            int unfairness = *max_element(begin(children), end(children));
+            result = min(result, unfairness);
+            return;
+        }
+
+        int curr_cookies = cookies[idx];
+
+        for (int i = 0; i < k; i++) {
+            children[i] += curr_cookies;  // Give cookies[idx] to child i
+            backtrack(cookies, children, k, idx + 1); //explore
+            children[i] -= curr_cookies;  // Backtrack
+        }
+    }
+
+    int distributeCookies(vector<int>& cookies, int k) {
+        n = cookies.size();
+        vector<int> children(k, 0);
+        backtrack(cookies, children, k, 0);
+        return result;
+    }
+};
