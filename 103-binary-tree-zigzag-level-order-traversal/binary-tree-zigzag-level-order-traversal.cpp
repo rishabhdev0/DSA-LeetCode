d98b576b1975193821e1a41c262f4ac3291894c1
rishabhdev0 <rishabh52003@gmail.com>
2025-08-02 17:20:11 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (48.10%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
@@ -1,40 +1,32 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-        vector<vector<int>>result;
-        if(root==nullptr) return result;
-        queue<TreeNode* > que;
-        que.push(root);
-       bool leftToright = true;
-       while(!que.empty()){
-        int level_size = que.size();
-        vector<int>level(level_size);
-        for(int i = 0 ; i<level_size;i++){
-            TreeNode* node = que.front();
-            que.pop();
-            int index = leftToright ? i : (level_size - 1 -i);
-            level[index] = node->val;
-            if(node->left !=  nullptr){
-                que.push(node->left);
+        vector<vector<int>> result;
+        if (!root) return result;
+
+        queue<TreeNode*> q;
+        q.push(root);
+        bool leftToRight = true;
+
+        while (!q.empty()) {
+            int levelSize = q.size();
+            vector<int> level(levelSize);
+
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode* node = q.front();
+                q.pop();
+
+                int index = leftToRight ? i : (levelSize - 1 - i);
+                level[index] = node->val;
+
+                if (node->left)  q.push(node->left);
+                if (node->right) q.push(node->right);
             }
-            if(node->right !=  nullptr){
-                que.push(node->right);
-            }
+
+            result.push_back(level);
+            leftToRight = !leftToRight; // flip direction
         }
-        result.push_back(level);
-        leftToright = !leftToright; // direction change
-       }
-       return result;
+
+        return result;
     }
 };
