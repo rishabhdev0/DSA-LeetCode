31a51be43861ddae52319f562ed6eaccfba78ebb
rishabhdev0 <rishabh52003@gmail.com>
2025-08-07 18:51:35 +0530

    Time: 0 ms (100.00%) | Memory: 14.2 MB (48.84%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
@@ -1,54 +1,17 @@
-/*
-
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-        int s = 0;
-        int e = nums.size() - 1;
-
-       
-        if (nums[s] <= nums[e]) {
-            return nums[s]; 
-        }
-
-        while (s <= e) {
-            int mid = s + (e - s) / 2;
-
+        int left = 0, right = nums.size() - 1;
+        
+        while(left < right) {
+            int mid = left + (right - left) / 2;
             
-            if ((mid == 0 || nums[mid] < nums[mid - 1]) && 
-                (mid == nums.size() - 1 || nums[mid] < nums[mid + 1])) {
-                return nums[mid];
-            }
-
-            
-            if ( nums[s]<= nums[mid]) {
-                s = mid+1 ; 
-            }
-          
-            else {
-                e = mid ; 
+            if(nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid; // mid can be answer
             }
         }
-
-        return 0; 
+        return nums[left];
     }
-};
-
-*/
-class Solution {
- public:
-  int findMin(vector<int>& nums) {
-    int s = 0;
-    int e = nums.size() - 1;
-
-    while (s < e) {
-       int mid = (s + e) / 2;
-      if (nums[mid] < nums[e])
-        e = mid;
-      else
-        s = mid + 1;
-    }
-
-    return nums[s];
-  }
 };
