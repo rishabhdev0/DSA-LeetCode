338028044ae7c690985835d5441b4aee5fb44512
rishabhdev0 <rishabh52003@gmail.com>
2025-08-11 19:43:35 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (40.06%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -1,34 +1,26 @@
 class Solution {
 public:
     vector<int> rightSideView(TreeNode* root) {
-        if (root == nullptr) {
-            return {};
-        }
-
+        if (!root) return {};
+        
         vector<int> result;
         queue<TreeNode*> que;
         que.push(root);
 
         while (!que.empty()) {
             int n = que.size();
-            TreeNode* node = nullptr;
-
             for (int i = 0; i < n; i++) {
-                node = que.front();
+                TreeNode* node = que.front();
                 que.pop();
 
-                if (node->left != nullptr) {
-                    que.push(node->left);
-                }
-                if (node->right != nullptr) {
-                    que.push(node->right);
+                if (node->left) que.push(node->left);
+                if (node->right) que.push(node->right);
+
+                if (i == n - 1) {  // last node make sure 
+                    result.push_back(node->val);
                 }
             }
-
-           // the nodes had the value of rightmost 
-            result.push_back(node->val);
         }
-
         return result;
     }
 };
