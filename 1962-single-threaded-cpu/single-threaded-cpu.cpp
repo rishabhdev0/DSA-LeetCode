9bfab7a1dc90419cf76a87d9571d6ea1a2dbf7d7
rishabhdev0 <rishabh52003@gmail.com>
2025-09-02 12:15:10 +0530

    Time: 79 ms (86.22%) | Memory: 131.9 MB (78.74%) - LeetSync

---
+++ 1962-single-threaded-cpu/single-threaded-cpu.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<int> getOrder(vector<vector<int>>& tasks) {
+        int n = tasks.size();
+        vector<array<int, 3>> SortedTask; // {enqueue_time, processing_time, index}
+
+        for (int i = 0; i < n; i++) {
+            SortedTask.push_back({tasks[i][0], tasks[i][1], i});
+        }
+
+        // Sort by enqueue_time
+        sort(SortedTask.begin(), SortedTask.end());
+
+        vector<int> result;
+        long long curr_time = 0;
+        int index = 0;
+
+        // Min-heap: {processing_time, index}
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+
+        while (index < n || !pq.empty()) {
+            // If CPU is idle, jump to the next available task
+            if (pq.empty() && curr_time < SortedTask[index][0]) {
+                curr_time = SortedTask[index][0];
+            }
+
+            // Push all tasks that can be started now
+            while (index < n && SortedTask[index][0] <= curr_time) {
+                pq.push({SortedTask[index][1], SortedTask[index][2]});
+                index++;
+            }
+
+            // Pick the task with smallest processing time (and smallest index if tie)
+            auto curr_task = pq.top();
+            pq.pop();
+
+            curr_time += curr_task.first; // add processing time
+            result.push_back(curr_task.second);
+        }
+
+        return result;
+    }
+};
