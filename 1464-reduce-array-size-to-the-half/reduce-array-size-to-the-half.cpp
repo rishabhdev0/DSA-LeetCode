b25e6e0e58f63572c186ddeda3e03d0daef085eb
rishabhdev0 <rishabh52003@gmail.com>
2025-09-08 18:23:37 +0530

    Time: 73 ms (69.47%) | Memory: 82.2 MB (70.71%) - LeetSync

--- 1464-reduce-array-size-to-the-half/reduce-array-size-to-the-half.cpp
+++ 1464-reduce-array-size-to-the-half/reduce-array-size-to-the-half.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
-    int minSetSize(vector<int>& arr) {
-       int n = arr.size();
-       unordered_map<int , int>freq;
-       for(int i =0 ; i <  n ; i++){
-        freq[arr[i]]++;
-       } 
-       // now using heap for the freq
-       priority_queue<int>pq;
-       for(auto it = freq.begin() ; it!=freq.end() ; it++){
-           pq.push(it->second);
-       }
-      //now finding the acyual result;
-      int count = 0;
-      int removed = 0; //since we remove the the freq we could get n-removed to half
-      int half = n/2;
-      while(removed < half){
-        removed +=pq.top(); // {3,4} so 4 element is being removed
-        pq.top();
-        pq.pop();
-        count++;
-      }
-      return count;
+    int minSetSize(vector<int>& nums) {
+        int n = nums.size();
+        unordered_map<int , int>freq;
+        for(int i = 0 ; i < n ; i++){
+            freq[nums[i]]++;
+        }
+
+        // building the heap 
+        priority_queue<int>pq;
+        for(auto it = freq.begin() ; it!=freq.end() ; it++){
+            pq.push(it->second); // freq based map heap
+        }
+        // building tht result 
+        int removed = 0;
+        int result = 0 ;
+        int half  = n/2;
+        while(removed < half){
+            removed +=  pq.top(); // {3,4 } --->4 will be added;
+            pq.top();
+            pq.pop();
+            result++;
+        }
+        return result;
     }
 };
