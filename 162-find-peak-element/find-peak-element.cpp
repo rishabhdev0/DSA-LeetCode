751604d11f566312212ff8f7347353ca388053cd
rishabhdev0 <rishabh52003@gmail.com>
2025-09-15 17:34:44 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (32.78%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -1,44 +1,13 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-        if(nums.size()==1) return 0;
-      int s=0;
-      int e=nums.size()-1;
-      while(s<=e){
-        int mid=s+(e-s)/2;
-           if ((mid == 0 || nums[mid] >= nums[mid - 1]) && (mid == nums.size() - 1 || 
-           nums[mid] >= nums[mid + 1])) {
-    return mid;
-}
-        else if(mid>0 && nums[mid-1]>nums[mid]){
-            e=mid-1;
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            if ((i == 0 || nums[i] > nums[i - 1]) && 
+                (i == n - 1 || nums[i] > nums[i + 1])) {
+                return i;  
+            }
         }
-        else{
-        s=mid+1;
-        }
-      }
-      return -1;
-         
-      
+        return -1; 
     }
 };
-
-
-// class Solution {
-// public:
-//     int findPeakElement(vector<int>& nums){
-//         if(nums.empty()) return -1;
-//          if(nums.size()==1) return  0;
-//           if (nums.size()== 2) {
-//             return (nums[0] > nums[1]) ? 0 : 1;  
-//         }
-
-//        for(int i=1;i<nums.size();i++){
-//         if (nums[i]<=nums[i-1]){
-//             return i-1;
-//         }
-        
-//        }
-//        return -1;
-//     }
-// };
