e5a3dae5c3ad90613f465a619c51723e1862f9cd
rishabhdev0 <rishabh52003@gmail.com>
2025-10-02 09:53:08 +0530

    Time: 135 ms (32.07%) | Memory: 114.7 MB (62.76%) - LeetSync

---
+++ 1612-avoid-flood-in-the-city/avoid-flood-in-the-city.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> avoidFlood(vector<int>& rains) {
+        int n = rains.size();
+        vector<int>result(n , -1);
+        unordered_map<int , int>LastRains; // rain , the last day it rained
+        set<int>Dry; // to check if i dry the lake;
+
+        for(int i = 0 ; i < n ; i++){
+             if(rains[i] == 0){
+                Dry.insert(i);
+                result[i] = 1; // by default , will change later;
+             }else {
+                // not a dry lake
+                int lake = rains[i];
+                if(LastRains.count(lake)){
+                    // lake found before , time to find a dry one
+                    auto it = Dry.upper_bound(LastRains[lake]);
+                    if(it == Dry.end()) return {};
+                    int DryDay = *it;
+                    result[DryDay] = lake;
+                    Dry.erase(DryDay);
+                }
+                LastRains[lake] = i;
+             }
+        }
+        return result;
+    }
+};
