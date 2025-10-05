9707a76fd8968a4ab9375d656202c8b55853c27b
rishabhdev0 <rishabh52003@gmail.com>
2025-10-05 11:35:47 +0530

    Time: 0 ms (100.00%) | Memory: 35.8 MB (91.57%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/find-k-closest-elements.cpp
@@ -1,30 +1,16 @@
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        int n = arr.size();
-        // Max heap to store pairs of (absolute difference, value)
-        priority_queue<pair<int, int>> pq;
+        int left = 0, right = arr.size() - 1;
         
-        for(int i = 0; i < n; i++){
-            int diff = abs(arr[i] - x);
-            pq.push({diff, arr[i]});
-            
-            // Maintain only k elements in heap
-            if(pq.size() > k){
-                pq.pop();
+        while (right - left >= k) {
+            if (abs(arr[left] - x) > abs(arr[right] - x)) {
+                left++;
+            } else {
+                right--;
             }
         }
         
-        vector<int> result;
-        
-        while(!pq.empty()){
-            auto top = pq.top();
-            pq.pop();
-            result.push_back(top.second); 
-        }
-        
-        // Sort the result as required by problem
-        sort(result.begin(), result.end());
-        return result;
+        return vector<int>(arr.begin() + left, arr.begin() + right + 1);
     }
 };
