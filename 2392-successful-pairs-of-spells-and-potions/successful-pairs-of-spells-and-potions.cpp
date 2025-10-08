036499f0a0e6fb61c0971dd93cc547116687df48
rishabhdev0 <rishabh52003@gmail.com>
2025-10-08 11:27:21 +0530

    Time: 44 ms (53.89%) | Memory: 125 MB (94.62%) - LeetSync

--- 2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.cpp
+++ 2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.cpp
@@ -1,38 +1,28 @@
 class Solution {
-private:
-    int lower_bound(int l, int r, vector<int>& potions, int minPortions) {
-        int possibleindex = -1;
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
-            if (potions[mid] >= minPortions) {
-                possibleindex = mid;
-                r = mid - 1;
-            } else {
-                l = mid + 1;
-            }
-        }
-        return possibleindex;
-    }
-
 public:
     vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
         int n = spells.size();
         int m = potions.size();
         sort(potions.begin(), potions.end());
-        int maxPotion = potions[m - 1];  
-        vector<int> answer;
-
-        for (int i = 0; i < n; i++) {  
-             long long minPortion = (success + spells[i] - 1) / spells[i];
-            if (minPortion > maxPotion) {
-                answer.push_back(0);
-                continue;
+        vector<int> result(n, 0);
+        for (int i = 0; i < n; i++) {
+            int left = 0;
+            int right = m - 1;            
+            int validIndex = -1;
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+                long long product = 1LL * spells[i] * potions[mid]; 
+                if (product >= success) {
+                    validIndex = mid;
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
             }
-            int index = lower_bound(0, m - 1, potions, minPortion);  
-            int count = (index == -1) ? 0 : m - index;
-            answer.push_back(count);
+            if(validIndex != -1){
+                result[i] = m - validIndex;
+            }
         }
-
-        return answer;
+        return result;
     }
 };
