b13b68494b27f0d3cb7602aa8aa211632fce9022
rishabhdev0 <rishabh52003@gmail.com>
2025-10-16 18:45:04 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (59.37%) - LeetSync

--- 226-invert-binary-tree/invert-binary-tree.cpp
+++ 226-invert-binary-tree/invert-binary-tree.cpp
@@ -1,27 +1,23 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     TreeNode* invertTree(TreeNode* root) {
-        if(root==nullptr) return nullptr;
-        //swap the children
-        TreeNode* temp = root->left;
-        root->left = root->right ;
-        root->right = temp;
-
-        invertTree(root->left);
-        invertTree(root->right);
-
+        if (root == nullptr) return nullptr;
+        
+        queue<TreeNode*> q;
+        q.push(root);
+        
+        while (!q.empty()) {
+            TreeNode* node = q.front();
+            q.pop();
+            
+            // Swap left and right children
+            swap(node->left, node->right);
+            
+            // Push non-null children into the queue
+            if (node->left) q.push(node->left);
+            if (node->right) q.push(node->right);
+        }
+        
         return root;
-
     }
 };
