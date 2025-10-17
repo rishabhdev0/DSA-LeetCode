5264384c15c78187fd1bb0747c69522a9cba1ce9
rishabhdev0 <rishabh52003@gmail.com>
2025-10-17 18:44:18 +0530

    Time: 7 ms (15.94%) | Memory: 20.6 MB (37.56%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,22 +1,51 @@
-class Solution {
-private:
-    void dfs(vector<vector<int>>& graph, vector<bool>& visited, int node) {
-        visited[node] = true;
-        for(int neighbor : graph[node]) {
-            if(!visited[neighbor]) {
-                dfs(graph, visited, neighbor);
-            }
-        }
-    }
+// class Solution {
+// private:
+//     void dfs(vector<vector<int>>& graph, vector<bool>& visited, int node) {
+//         visited[node] = true;
+//         for(int neighbor : graph[node]) {
+//             if(!visited[neighbor]) {
+//                 dfs(graph, visited, neighbor);
+//             }
+//         }
+//     }
    
+// public:
+//     int findCircleNum(vector<vector<int>>& isConnected) {
+//         int n = isConnected.size();
+//         vector<vector<int>> graph(n);
+//         // Build adjacency list from adjacency matrix
+//         for(int i = 0; i < n; i++) {
+//             for(int j = 0; j < n; j++) {
+//                 if(isConnected[i][j] == 1 && i != j) {
+//                     graph[i].push_back(j);
+//                 }
+//             }
+//         }
+        
+//         int count = 0;
+//         vector<bool> visited(n, false);
+        
+//         for(int i = 0; i < n; i++) {
+//             if(!visited[i]) {
+//                 count++; // New province found
+//                 dfs(graph, visited, i);
+//             }
+//         }
+        
+//         return count;
+//     }
+// };
+
+class Solution {
 public:
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
-        vector<vector<int>> graph(n);
-        // Build adjacency list from adjacency matrix
+        vector<vector<int>> graph(n);  
+        
+        // Build graph from adjacency matrix
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < n; j++) {
-                if(isConnected[i][j] == 1 && i != j) {
+                if(isConnected[i][j] == 1) {
                     graph[i].push_back(j);
                 }
             }
@@ -24,11 +53,25 @@ public:
         
         int count = 0;
         vector<bool> visited(n, false);
-        
+    
         for(int i = 0; i < n; i++) {
             if(!visited[i]) {
-                count++; // New province found
-                dfs(graph, visited, i);
+                count++;
+                queue<int> q;
+                q.push(i);
+                visited[i] = true;
+                
+                while(!q.empty()) {
+                    int node = q.front();  
+                    q.pop();
+                    
+                    for(int neighbor : graph[node]) {
+                        if(!visited[neighbor]) {
+                            visited[neighbor] = true;
+                            q.push(neighbor);
+                        }
+                    }
+                }
             }
         }
         
