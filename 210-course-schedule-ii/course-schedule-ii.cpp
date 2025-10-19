4a86edd24506a1759eecdf9632b8eed12aa76848
rishabhdev0 <rishabh52003@gmail.com>
2025-10-19 17:54:27 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (82.07%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,42 +1,83 @@
-class Solution {
-private:
-    bool dfs(int node, vector<bool>& visited, vector<bool>& inRecursion, vector<vector<int>>& graph, vector<int>& result) {
-        visited[node] = true;
-        inRecursion[node] = true;
-        for(auto neighbor : graph[node]) {
-            if(!visited[neighbor]) {
-                if(dfs(neighbor, visited, inRecursion, graph, result)) {
-                    return true;
-                }
-            }
-            else if(inRecursion[neighbor]) {
-                return true; 
-            }
-        }
-        inRecursion[node] = false; // Backtrack
-        result.push_back(node); // Post-order
-        return false;
-    }
+// class Solution {
+// private:
+//     bool dfs(int node, vector<bool>& visited, vector<bool>& inRecursion, vector<vector<int>>& graph, vector<int>& result) {
+//         visited[node] = true;
+//         inRecursion[node] = true;
+//         for(auto neighbor : graph[node]) {
+//             if(!visited[neighbor]) {
+//                 if(dfs(neighbor, visited, inRecursion, graph, result)) {
+//                     return true;
+//                 }
+//             }
+//             else if(inRecursion[neighbor]) {
+//                 return true; 
+//             }
+//         }
+//         inRecursion[node] = false; // Backtrack
+//         result.push_back(node); // Post-order
+//         return false;
+//     }
     
+// public:
+//     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+//         vector<vector<int>> graph(numCourses);
+//         vector<bool> visited(numCourses, false);
+//         vector<bool> inRecursion(numCourses, false);
+//         vector<int> result;
+        
+//         for(auto &pre : prerequisites) {
+//             graph[pre[1]].push_back(pre[0]);
+//         }
+        
+//         for(int i = 0; i < numCourses; i++) {
+//             if(!visited[i]) {
+//                 if(dfs(i, visited, inRecursion, graph, result)) {
+//                     return {}; 
+//                 }
+//             }
+//         }
+//         reverse(result.begin(), result.end());
+//         return result;
+//     }
+// };
+
+
+// breadth first search;
+class Solution {
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>> graph(numCourses);
-        vector<bool> visited(numCourses, false);
-        vector<bool> inRecursion(numCourses, false);
+        vector<int> indegree(numCourses, 0);
         vector<int> result;
-        
         for(auto &pre : prerequisites) {
             graph[pre[1]].push_back(pre[0]);
+            indegree[pre[0]]++;
         }
         
+        queue<int> q;
         for(int i = 0; i < numCourses; i++) {
-            if(!visited[i]) {
-                if(dfs(i, visited, inRecursion, graph, result)) {
-                    return {}; 
+            if(indegree[i] == 0) {
+                q.push(i);
+            }
+        }
+        
+        while(!q.empty()) {
+            int node = q.front();
+            q.pop();
+            result.push_back(node);
+            // Process neighbors
+            for(int neighbor : graph[node]) {
+                indegree[neighbor]--;
+                if(indegree[neighbor] == 0) {
+                    q.push(neighbor);
                 }
             }
         }
-        reverse(result.begin(), result.end());
-        return result;
+        if(result.size() == numCourses) {
+            return result;
+        } else {
+            return {};
+        }
     }
 };
+
