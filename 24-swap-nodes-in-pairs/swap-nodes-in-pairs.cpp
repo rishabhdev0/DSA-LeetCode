c849b1a2216b09b7bb84b49e458c77962dc2e14f
rishabhdev0 <rishabh52003@gmail.com>
2025-11-03 19:16:21 +0530

    Time: 0 ms (100.00%) | Memory: 11.3 MB (19.32%) - LeetSync

--- 24-swap-nodes-in-pairs/swap-nodes-in-pairs.cpp
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.cpp
@@ -11,14 +11,12 @@
 class Solution {
 public:
     ListNode* swapPairs(ListNode* head) {
-        if(head==nullptr || head->next == nullptr){
-            return head;
-        }
-    ListNode* first = head;
-    ListNode* second = head->next;
-
-    first->next = swapPairs(second->next);
-    second->next = first;
-return second;
+       // base case
+       if(head == nullptr || head->next == nullptr) return head;
+       ListNode* first = head;
+       ListNode* second = head->next;
+       first->next = swapPairs(second->next);
+       second -> next = first;
+       return second; 
     }
 };
