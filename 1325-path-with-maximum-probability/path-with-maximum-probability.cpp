488eb5d655109d9f715932c1b4bbf6a089d1dd5f
rishabhdev0 <rishabh52003@gmail.com>
2025-11-06 11:28:12 +0530

    Time: 52 ms (54.54%) | Memory: 70.9 MB (57.57%) - LeetSync

---
+++ 1325-path-with-maximum-probability/path-with-maximum-probability.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start_node, int end_node) {
+        // Build graph
+        vector<vector<pair<int, double>>> graph(n);
+        for(int i = 0; i < edges.size(); i++) {
+            int u = edges[i][0];
+            int v = edges[i][1];
+            double prob = succProb[i];
+            graph[u].push_back({v, prob});
+            graph[v].push_back({u, prob});
+        }
+        vector<double> prob(n, 0.0);
+        prob[start_node] = 1.0;
+        
+        priority_queue<pair<double, int>> pq;
+        pq.push({1.0, start_node});
+        
+        while(!pq.empty()) {
+            double current_prob = pq.top().first;
+            int node = pq.top().second;
+            pq.pop();
+            
+            // If we have a better probability already, skip
+            if(current_prob < prob[node]) continue;
+            
+            // Explore neighbors
+            for(auto& neighbor : graph[node]) {
+                int next_node = neighbor.first;
+                double edge_prob = neighbor.second;
+                double new_prob = current_prob * edge_prob;
+                
+                if(new_prob > prob[next_node]) {
+                    prob[next_node] = new_prob;
+                    pq.push({new_prob, next_node});
+                }
+            }
+        }
+        
+        return prob[end_node];
+    }
+};
