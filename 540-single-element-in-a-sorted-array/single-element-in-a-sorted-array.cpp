9594a34e3794b2ebb5321adf9ede8b7c4b7ba4dd
rishabhdev0 <rishabh52003@gmail.com>
2025-11-13 12:37:09 +0530

    Time: 0 ms (100.00%) | Memory: 26.3 MB (33.41%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -1,10 +1,22 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        int result = 0;
-        for(int num : nums){
-            result^=num;
+        int start = 0;
+        int end = nums.size() - 1;
+        
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (mid % 2 == 1) mid--; // we always check even indices
+            
+            // If pairs match, single element is to the right
+            if (nums[mid] == nums[mid + 1]) {
+                start = mid + 2;
+            } else {
+                // Single element is at mid or to the left
+                end = mid;
+            }
         }
-        return result;
+        
+        return nums[start];
     }
 };
