c2836d3bb2209751de06d7b7a9871c848f3a55e9
rishabhdev0 <rishabh52003@gmail.com>
2025-11-13 14:22:42 +0530

    Time: 0 ms (100.00%) | Memory: 14.6 MB (57.44%) - LeetSync

--- 871-keys-and-rooms/keys-and-rooms.cpp
+++ 871-keys-and-rooms/keys-and-rooms.cpp
@@ -1,20 +1,50 @@
+// class Solution {
+// private:
+//     void dfs(int source, vector<vector<int>>& rooms, vector<bool>& visited) {
+//         visited[source] = true;
+//         for(auto neighbor : rooms[source]) {
+//             if(!visited[neighbor]) {
+//                 dfs(neighbor, rooms, visited);
+//             }
+//         }
+//     }
+// public:
+//     bool canVisitAllRooms(vector<vector<int>>& rooms) {
+//         int n = rooms.size();
+//         vector<bool> visited(n, false);
+//         dfs(0, rooms, visited);
+//         for(int i = 0; i < n; i++) {
+//             if(visited[i] == false) return false;
+//         }
+//         return true;
+//     }
+// };
+
 class Solution {
-private:
-    void dfs(int source, vector<vector<int>>& rooms, vector<bool>& visited) {
-        visited[source] = true;
-        for(auto neighbor : rooms[source]) {
-            if(!visited[neighbor]) {
-                dfs(neighbor, rooms, visited);
-            }
-        }
-    }
 public:
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
         int n = rooms.size();
+        queue<int> q;
         vector<bool> visited(n, false);
-        dfs(0, rooms, visited);
-        for(int i = 0; i < n; i++) {
-            if(visited[i] == false) return false;
+        
+        q.push(0);
+        visited[0] = true;
+        
+        while (!q.empty()) {
+            int room = q.front();
+            q.pop();
+            
+            for (int neighbor : rooms[room]) {
+                if (!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    q.push(neighbor);
+                }
+            }
+        }
+        
+        // Check if all rooms are visited
+        for (bool v : visited) {
+            if (!v) return false;
         }
         return true;
     }
