bfb94e52ad9e62ea5a2e2cccffdb7329c340fda1
rishabhdev0 <rishabh52003@gmail.com>
2025-11-15 13:03:20 +0530

    Time: 0 ms (100.00%) | Memory: 22.8 MB (76.88%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,41 +1,39 @@
 class Solution {
-public:
-int find_max(vector<int>piles){
-    int maxi=INT_MIN;
-    int n=piles.size();
-    for(int i=0;i<n;i++){
-        maxi=max(piles[i],maxi);
-    }
-    return maxi;
-}
-
-
-
-    int check(vector<int>& piles, int h, int mid) {
-        int ans = 0;
-        for (int i = 0; i < piles.size(); i++) {
-            ans += (piles[i] / mid);
-            if (piles[i] % mid != 0) {
-                ans++;
+private:
+    int find_max(vector<int>& piles) {
+        int maxi = INT_MIN;
+        for(int i = 0; i < piles.size(); i++) {
+            if(piles[i] > maxi) {
+                maxi = piles[i];
             }
         }
-        return ans <= h;
+        return maxi;
     }
 
+    bool canEatInTime(vector<int>& piles, int k, int h) {
+        long long hours = 0; 
+        for(int i = 0; i < piles.size(); i++) {
+            hours += ceil((double)piles[i] / k);
+            if(hours > h) return false; 
+        }
+        return hours <= h;
+    }
 
+public:
     int minEatingSpeed(vector<int>& piles, int h) {
-        int n = piles.size();
         int low = 1;
         int high = find_max(piles);
-
-        while (low < high) {
-            int mid = (low + high) >> 1;
-            if (check(piles, h, mid)) {
-                high = mid;
+        int result = -1;
+        
+        while(low <= high) {
+            int mid = low + (high - low) / 2;
+            if(canEatInTime(piles, mid, h)) {
+                result = mid;
+                high = mid - 1;
             } else {
                 low = mid + 1;
             }
         }
-        return low;
+        return result;
     }
 };
