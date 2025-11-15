57deceb8c32946e954cb5bb5c8c15bce2928f92f
rishabhdev0 <rishabh52003@gmail.com>
2025-11-15 13:24:13 +0530

    Time: 8 ms (82.97%) | Memory: 35 MB (85.07%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -1,38 +1,44 @@
 class Solution {
 private:
-    bool can_ship(vector<int>& weights, int days, int capacity) {
-        int load = 0;
-        int day_count = 1;
-
-        for (int weight : weights) {
-            if (load + weight > capacity) {
-                day_count++;      
-                load = weight;    
-            } else {
-                load += weight;
+    bool canShip(vector<int>& weights, int capacity, int days) {
+        int currentLoad = 0;
+        int requiredDays = 1; 
+        
+        for(int weight : weights) {
+            // If adding this weight exceeds capacity, we need a new day
+            if(currentLoad + weight > capacity) {
+                requiredDays++;
+                currentLoad = 0;
+                
+                // If we've exceeded the allowed days, return false
+                if(requiredDays > days) {
+                    return false;
+                }
             }
+            currentLoad += weight;
         }
-
-        return day_count <= days; 
+        
+        return requiredDays <= days;
     }
 
 public:
     int shipWithinDays(vector<int>& weights, int days) {
-        int left = *max_element(weights.begin(), weights.end());
-        int right = accumulate(weights.begin(), weights.end(), 0);
-        int result = right;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-
-            if (can_ship(weights, days, mid)) {
+        int low = *max_element(weights.begin(), weights.end());
+        int high = accumulate(weights.begin(), weights.end(), 0);
+        
+        int result = -1;
+        
+        while(low <= high) {
+            int mid = low + (high - low) / 2;
+            
+            if(canShip(weights, mid, days)) {
                 result = mid;
-                right = mid - 1;  
+                high = mid - 1;
             } else {
-                left = mid + 1;  
+                low = mid + 1; // Need larger capacity
             }
         }
-
+        
         return result;
     }
 };
