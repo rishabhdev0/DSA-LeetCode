51468d41f66574825ee9a412a4f10c02d411b081
rishabhdev0 <rishabh52003@gmail.com>
2025-11-18 22:35:43 +0530

    Time: 67 ms (43.72%) | Memory: 98.4 MB (87.15%) - LeetSync

--- 2294-minimum-time-to-complete-trips/minimum-time-to-complete-trips.cpp
+++ 2294-minimum-time-to-complete-trips/minimum-time-to-complete-trips.cpp
@@ -1,29 +1,30 @@
 class Solution {
 private:
-    bool ispossible(vector<int>& time, int totalTrips, long long giventime) {
-        long long actualtrips = 0;
-        for (int t : time) {
-            actualtrips += giventime / t;
+    bool canTravel(vector<int>& time, int totalTrips, long long mid) {
+        long long trips = 0;
+        for(int t : time){
+            trips += mid / t;  
         }
-        return actualtrips >= totalTrips;
+        return trips >= totalTrips; 
     }
 
 public:
     long long minimumTime(vector<int>& time, int totalTrips) {
-        long long left = 1;
-        long long right = (long long) *max_element(time.begin(), time.end()) * totalTrips; 
-        long long result = right;
-
-        while (left <= right) {
-            long long mid = left + (right - left) / 2;
-            if (ispossible(time, totalTrips, mid)) {
+        long long result = -1;
+        long long low = 1;
+        long long high = (long long)*max_element(time.begin(), time.end()) * totalTrips;
+        
+        while(low <= high){
+            long long mid = low + (high - low) / 2;
+            
+            if(canTravel(time, totalTrips, mid)){
                 result = mid;
-                right = mid - 1;  
+                high = mid - 1;  
             } else {
-                left = mid + 1;
+                low = mid + 1;   
             }
         }
-
+        
         return result;
     }
 };
