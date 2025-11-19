5363e1e7699d6bca05ae66cf0fd9e0101bd50083
rishabhdev0 <rishabh52003@gmail.com>
2025-11-19 20:08:34 +0530

    Time: 23 ms (72.02%) | Memory: 88.3 MB (31.01%) - LeetSync

--- 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.cpp
+++ 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.cpp
@@ -1,12 +1,5 @@
 class Solution {
 private:
-    int find_max(const vector<int>& candies) {
-        int maxi = INT_MIN;
-        for (int candy : candies) {
-            maxi = max(candy, maxi);  
-        }
-        return maxi;
-    }
     bool ispossible(const vector<int>& candies, int divisor, long long k) {
         long long allocate = 0;
         for (int candy : candies) {
@@ -19,7 +12,7 @@ public:
     int maximumCandies(vector<int>& candies, long long k) {
         int result = 0;
         int s = 1;  
-        int e = find_max(candies);  
+        int e = *max_element(candies.begin() , candies.end());  
         while (s <= e) {
             int mid = s + (e - s) / 2;
             if (ispossible(candies, mid, k)) {
