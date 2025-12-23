81d4a6abf8dc8ee3f7697bc13ee78d1fa93e54a9
rishabhdev0 <rishabh52003@gmail.com>
2025-12-23 13:40:53 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (58.97%) - LeetSync

--- 1693-sum-of-all-odd-length-subarrays/sum-of-all-odd-length-subarrays.cpp
+++ 1693-sum-of-all-odd-length-subarrays/sum-of-all-odd-length-subarrays.cpp
@@ -1,18 +1,36 @@
+// class Solution {
+// public:
+//     int sumOddLengthSubarrays(vector<int>& arr) {
+//         int n = arr.size();
+//         int oddSum = 0;
+
+//         for (int i = 0; i < n; i++) {
+//             int currSum = 0;
+//             for (int j = i; j < n; j++) {
+//                 currSum += arr[j]; 
+//                 if ((j - i + 1) % 2 != 0) {
+//                     oddSum += currSum;
+//                 }
+//             }
+//         }
+//         return oddSum;
+//     }
+// };
+
+// using more optimal method
+
 class Solution {
 public:
     int sumOddLengthSubarrays(vector<int>& arr) {
         int n = arr.size();
-        int oddSum = 0;
-
-        for (int i = 0; i < n; i++) {
-            int currSum = 0;
-            for (int j = i; j < n; j++) {
-                currSum += arr[j]; 
-                if ((j - i + 1) % 2 != 0) {
-                    oddSum += currSum;
-                }
-            }
+        int odd_count = 0;
+        for(int i = 0 ; i < n ; i++){
+            // find the total subarray that include the ith position
+            int total = (i + 1) * (n - i);
+            // find how many of them are odd
+            int odd = (total + 1) /2;
+            odd_count += odd * arr[i];
         }
-        return oddSum;
+        return odd_count;
     }
-};
+};    
