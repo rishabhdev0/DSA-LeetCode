ae8a59a4e06475564049bdf30e7a415fa7021a7c
Rishabh pandey <rishabh52003@gmail.com>
2026-02-01 10:28:48 +0530

    Time: 1 ms (28.57%) | Memory: 9.4 MB (16.27%) - LeetSync

---
+++ 409-longest-palindrome/longest-palindrome.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        unordered_map<char, int> freq;
+        
+        for (char ch : s) {
+            freq[ch]++;
+        }
+
+        int result = 0;
+        bool hasOdd = false;
+
+        for (auto it = freq.begin(); it != freq.end(); it++) {
+            if (it->second % 2 == 0) {
+                result += it->second;
+            } else {
+                result += it->second - 1;
+                hasOdd = true;
+            }
+        }
+
+        if (hasOdd) result += 1;
+
+        return result;
+    }
+};
