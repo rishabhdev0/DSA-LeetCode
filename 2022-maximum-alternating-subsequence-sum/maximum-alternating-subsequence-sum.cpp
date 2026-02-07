eb2eb3ed6cd54fd9019f3fa9c0989c3a607b1adc
Rishabh pandey <rishabh52003@gmail.com>
2026-02-07 09:41:27 +0530

    Time: 0 ms (100.00%) | Memory: 157.2 MB (38.22%) - LeetSync

--- 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.cpp
+++ 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.cpp
@@ -1,28 +1,49 @@
+// class Solution {
+// public:
+//     long long dp[100001][2];
+
+//     long long solve(vector<int>& nums, int idx, bool even) {
+//         if (idx == nums.size()) return 0;
+
+//         if (dp[idx][even] != -1)
+//             return dp[idx][even];
+
+//         // option 1: skip current element
+//         long long skip = solve(nums, idx + 1, even);
+
+//         // option 2: take current element
+//         long long take;
+//         if (even)
+//             take = nums[idx] + solve(nums, idx + 1, !even);
+//         else
+//             take = -nums[idx] + solve(nums, idx + 1, !even);
+
+//         return dp[idx][even] = max(take, skip);
+//     }
+
+//     long long maxAlternatingSum(vector<int>& nums) {
+//         memset(dp, -1, sizeof(dp));
+//         return solve(nums, 0, true); // start with even
+//     }
+// };
+
+// bottom - up -> tabulation
+
 class Solution {
 public:
-    long long dp[100001][2];
-
-    long long solve(vector<int>& nums, int idx, bool even) {
-        if (idx == nums.size()) return 0;
-
-        if (dp[idx][even] != -1)
-            return dp[idx][even];
-
-        // option 1: skip current element
-        long long skip = solve(nums, idx + 1, even);
-
-        // option 2: take current element
-        long long take;
-        if (even)
-            take = nums[idx] + solve(nums, idx + 1, !even);
-        else
-            take = -nums[idx] + solve(nums, idx + 1, !even);
-
-        return dp[idx][even] = max(take, skip);
-    }
-
     long long maxAlternatingSum(vector<int>& nums) {
-        memset(dp, -1, sizeof(dp));
-        return solve(nums, 0, true); // start with even
+        int n = nums.size();
+        vector<vector<long long>> dp(n + 1, vector<long long>(2, 0));
+
+        for (int i = 1; i <= n; i++) {
+            dp[i][0] = max(dp[i-1][0], dp[i-1][1] + nums[i-1]); // even
+            dp[i][1] = max(dp[i-1][1], dp[i-1][0] - nums[i-1]); // odd
+        }
+
+        return dp[n][0];
     }
 };
+
+
+auto init = atexit([]() { ofstream("display_runtime.txt") << "000"; });
+
