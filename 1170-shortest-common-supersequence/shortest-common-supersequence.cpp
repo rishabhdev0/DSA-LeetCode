cf95c7564562309eeb2984d9cb2f4ab93ddcc651
Rishabh pandey <rishabh52003@gmail.com>
2026-02-16 17:03:34 +0530

    Time: 27 ms (37.71%) | Memory: 26.8 MB (42.84%) - LeetSync

---
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    string shortestCommonSupersequence(string s1, string s2) {
+        int n = s1.length();
+        int m = s2.length();
+
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (s1[i - 1] == s2[j - 1]) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        // for the printing a value;
+        int i = n, j = m;
+        string result = "";
+
+        while (i > 0 && j > 0) {
+            if (s1[i - 1] == s2[j - 1]) {
+                result.push_back(s1[i - 1]);
+                i--;
+                j--;
+            } else if (dp[i - 1][j] > dp[i][j - 1]) {
+                result.push_back(s1[i - 1]);
+                i--;
+            } else {
+                result.push_back(s2[j - 1]);
+                j--;
+            }
+        }
+
+        while (i > 0) {
+            result.push_back(s1[i - 1]);
+            i--;
+        }
+
+        while (j > 0) {
+            result.push_back(s2[j - 1]);
+            j--;
+        }
+
+        reverse(result.begin(), result.end());
+        return result;
+    }
+};
