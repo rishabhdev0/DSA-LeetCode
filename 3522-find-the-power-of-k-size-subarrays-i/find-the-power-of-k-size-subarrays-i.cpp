86f4f55bd1ee673a0622da7da52b8bd21d0e3964
Rishabh pandey <rishabh52003@gmail.com>
2026-02-21 10:48:33 +0530

    Time: 0 ms (100.00%) | Memory: 34.1 MB (90.21%) - LeetSync

--- 3522-find-the-power-of-k-size-subarrays-i/find-the-power-of-k-size-subarrays-i.cpp
+++ 3522-find-the-power-of-k-size-subarrays-i/find-the-power-of-k-size-subarrays-i.cpp
@@ -1,26 +1,31 @@
 class Solution {
 public:
-  bool isSorted(vector<int>& nums , int left , int right){
-    for(int i = left + 1 ; i <= right ; i++){
-        if(nums[i] != nums[i-1] + 1){
-            return false;
-        }
-    }
-    return true;
-  }
     vector<int> resultsArray(vector<int>& nums, int k) {
         int n = nums.size();
         vector<int>result(n - k + 1 , -1);
-        int i = 0;
-        int j = 0;
-        while(j < n){
-            if(j - i + 1 > k)i++;
-            if(j - i + 1 == k){
-                if(isSorted(nums , i , j)){
-                    result[i] = nums[j];
-                }
-                i++;
+        int count = 1;
+        for(int i = 1 ; i < k ; i++){
+            if(nums[i] == nums[i-1] + 1){
+               count++;
+            }else{
+                count = 1;
             }
+        }
+        if(count == k){
+            result[0] = nums[k-1];
+        }
+        int i = 1;
+        int j = k;
+        while(j < n){
+            if(nums[j] == nums[j-1] + 1){
+                count++;
+            }else{
+                count = 1;
+            }
+            if(count >= k){
+                result[i] = nums[j];
+            }
+            i++;
             j++;
         }
         return result;
