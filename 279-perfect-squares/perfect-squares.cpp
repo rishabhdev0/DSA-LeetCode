ac4dc14ce74640456e4ecf08698dd9ebbd6bf94e
Rishabh pandey <rishabh52003@gmail.com>
2026-02-24 11:26:22 +0530

    Time: 42 ms (73.10%) | Memory: 13.1 MB (68.72%) - LeetSync

--- 279-perfect-squares/perfect-squares.cpp
+++ 279-perfect-squares/perfect-squares.cpp
@@ -1,24 +1,40 @@
+// class Solution {
+// public:
+//     int dp[10001];
+
+//     int solve(int n) {
+//         if (n == 0) return 0;
+
+//         if (dp[n] != -1)
+//             return dp[n];
+
+//         int ans = INT_MAX;
+
+//         for (int i = 1; i * i <= n; i++) {
+//             ans = min(ans, 1 + solve(n - i * i));
+//         }
+
+//         return dp[n] = ans;
+//     }
+
+//     int numSquares(int n) {
+//         memset(dp, -1, sizeof(dp));
+//         return solve(n);
+//     }
+// };
+
+// bottom up -> tabulation
+
 class Solution {
 public:
-    int dp[10001];
-
-    int solve(int n) {
-        if (n == 0) return 0;
-
-        if (dp[n] != -1)
-            return dp[n];
-
-        int ans = INT_MAX;
-
-        for (int i = 1; i * i <= n; i++) {
-            ans = min(ans, 1 + solve(n - i * i));
-        }
-
-        return dp[n] = ans;
-    }
-
     int numSquares(int n) {
-        memset(dp, -1, sizeof(dp));
-        return solve(n);
+        vector<int>dp(n+1 , INT_MAX);
+        dp[0] = 0;
+        for(int i = 1 ; i <= n ; i++){
+            for(int j = 1 ; j * j <= i ; j++){
+                dp[i] = min(dp[i] ,1 + dp[i - j * j]);
+            }
+        }
+        return dp[n];
     }
 };
