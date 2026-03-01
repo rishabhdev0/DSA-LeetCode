3d128efef34d1c6ce54a1fc6c6bc04d4a487ca6a
Rishabh pandey <rishabh52003@gmail.com>
2026-03-01 19:59:55 +0530

    Time: 68 ms (21.01%) | Memory: 94.4 MB (13.65%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
@@ -2,28 +2,19 @@ class Solution {
 public:
     int numberOfSubarrays(vector<int>& nums, int k) {
         int n = nums.size();
-        int odd_Count = 0;
-        int result = 0;
-        int i = 0;
+        unordered_map<int , int>mp; // -> {odd , freq};
+        mp[0] = 1;
+        int odd = 0;
         int count = 0;
-
-        for (int j = 0; j < n; j++) {
-            if (nums[j] % 2 != 0) {
-                odd_Count++;
-                count = 0; 
+        for(int i = 0 ; i < n ; i++){
+            if(nums[i] % 2 == 1){
+                odd++;
             }
-
-            while (odd_Count == k) {
-                count++;
-                if (nums[i] % 2 != 0) {
-                    odd_Count--;
-                }
-                i++;
+            if(mp.find(odd- k) != mp.end()){
+                count += mp[odd - k];
             }
-
-            result += count;
+            mp[odd]++;
         }
-
-        return result;
+        return count;
     }
 };
