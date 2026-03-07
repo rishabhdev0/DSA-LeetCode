a83c554e9e341c513fce5ba7fb0096b605d26111
Rishabh pandey <rishabh52003@gmail.com>
2026-03-07 12:31:45 +0530

    Time: 3 ms (67.08%) | Memory: 10.9 MB (69.97%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -2,24 +2,21 @@ class Solution {
 public:
     int characterReplacement(string s, int k) {
         int n = s.length();
-        vector<int> freq(26, 0);
+        vector<int>freq(26 , 0);
         int max_len = 0;
-        int max_count = 0;
-        int i = 0, j = 0;
-
-        while (j < n) {
+        int i = 0;
+        int j = 0;
+        int maxCount = 0;
+        while(j < n){
             freq[s[j] - 'A']++;
-            max_count = max(max_count, freq[s[j] - 'A']);
-
-            if (j - i + 1 - max_count > k) {
+            maxCount = max(maxCount , freq[s[j] - 'A']);
+            if(j - i + 1 - maxCount > k){
                 freq[s[i] - 'A']--;
                 i++;
             }
-
-            max_len = max(max_len, j - i + 1);
+            max_len = max(max_len , j - i + 1);
             j++;
         }
-
         return max_len;
     }
 };
