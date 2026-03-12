77ce94ebc9065e236ea116691c137b63cf5b9aee
Rishabh pandey <rishabh52003@gmail.com>
2026-03-12 18:52:25 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (31.93%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -3,34 +3,27 @@ public:
     bool checkInclusion(string s1, string s2) {
         int n = s1.length();
         int m = s2.length();
-        if (n > m) return false; //edge case s1 cant bigger than s2
-
-        vector<int> freq(26, 0);
-        vector<int> windowfreq(26, 0);
-
-        // freq of s1
-        for (char &ch : s1) {
+        
+        vector<int>freq(26 , 0);
+        vector<int>win_freq(26 , 0);
+        for(auto &ch : s1){
             freq[ch - 'a']++;
         }
 
         int i = 0;
         int j = 0;
-        while (j < m) {
-            windowfreq[s2[j] - 'a']++; // current element to the windowfreq
 
-            if (j - i + 1 > n) {
-                windowfreq[s2[i] - 'a']--; // remove the ith charcter 
+        while(j < m){
+            win_freq[s2[j] - 'a']++;
+            if(j - i + 1 > n){
+                win_freq[s2[i] -'a']--;
                 i++;
             }
-
-           // freq must be equal  to window so that all char matched
-            if (j - i + 1 == n && freq == windowfreq) {
+            if(j - i + 1 == n && win_freq == freq){
                 return true;
             }
-
             j++;
         }
-
         return false;
     }
 };
