59866e83cf4c6ff0f96c57cf6190da7ac6f4794b
Rishabh pandey <rishabh52003@gmail.com>
2026-03-13 19:54:04 +0530

    Time: 87 ms (73.74%) | Memory: 13.5 MB (86.00%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,26 +1,53 @@
+// class Solution {
+// public:
+//      int dp[201][20001];
+
+//      bool solve(vector<int>& nums , int target , int curr , int idx){
+//         if(curr == target) return true;
+//         if(idx >= nums.size() || curr > target) return false;
+
+//         if(dp[idx][curr] != -1){
+//             return dp[idx][curr];
+//         }
+
+//       bool take = solve(nums , target  , curr + nums[idx] , idx + 1);
+//       bool skip = solve(nums , target , curr , idx +1);
+
+//       return dp[idx][curr] = take || skip;
+
+//      }
+
+//     bool canPartition(vector<int>& nums) {
+//         int total = accumulate(nums.begin() , nums.end() , 0);
+//         if(total % 2 != 0) return false;
+//         memset(dp , -1 , sizeof(dp));
+//         return solve(nums , total / 2 , 0 , 0);
+//     }
+// };
+
+
+// tabulation -> bottom up approach
+
 class Solution {
 public:
-     int dp[201][20001];
+    bool canPartition(vector<int>& nums) {
 
-     bool solve(vector<int>& nums , int target , int curr , int idx){
-        if(curr == target) return true;
-        if(idx >= nums.size() || curr > target) return false;
+        int total = accumulate(nums.begin(), nums.end(), 0);
 
-        if(dp[idx][curr] != -1){
-            return dp[idx][curr];
+        if(total % 2 != 0) return false;
+
+        int target = total / 2;
+
+        vector<bool> dp(target + 1, false);
+
+        dp[0] = true;
+
+        for(int num : nums){
+            for(int j = target; j >= num; j--){
+                dp[j] = dp[j] || dp[j - num];
+            }
         }
 
-      bool take = solve(nums , target  , curr + nums[idx] , idx + 1);
-      bool skip = solve(nums , target , curr , idx +1);
-
-      return dp[idx][curr] = take || skip;
-
-     }
-
-    bool canPartition(vector<int>& nums) {
-        int total = accumulate(nums.begin() , nums.end() , 0);
-        if(total % 2 != 0) return false;
-        memset(dp , -1 , sizeof(dp));
-        return solve(nums , total / 2 , 0 , 0);
+        return dp[target];
     }
 };
