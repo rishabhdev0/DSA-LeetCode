8d389bafd82ba7b1e4da3ba57e5dee84301f9857
Rishabh pandey <rishabh52003@gmail.com>
2026-03-17 19:44:07 +0530

    Time: 2 ms (28.91%) | Memory: 8.8 MB (27.15%) - LeetSync

--- 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.cpp
+++ 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.cpp
@@ -1,32 +1,20 @@
 class Solution {
-    private:
-    bool all_unique(const vector<int>&freq){
-        int unique_count = 0;
-         for(int i=0;i<26;i++){
-            if(freq[i] > 1){
-                return false;
-            }
-            
-        }
-        return true;
-    }
 public:
     int countGoodSubstrings(string s) {
-        int n =s.length();
-        vector<int> freq(26 , 0);
-        int count =  0;
-        int i = 0 ;
-        int j = 0;
+        int n = s.length();
         int k = 3;
-        while(j<n){
-            char curr = s[j];
-            freq[curr-'a']++;
-            while(j-i+1==k){
-               if(all_unique(freq)){
-                count++;
-               }
-               freq[s[i]-'a']--;
-               i++;
+        unordered_map<char,int> freq;
+        int i = 0, j = 0, count = 0;
+
+        while (j < n) {
+            freq[s[j]]++;
+            if (j - i + 1 == k) {
+                if (freq.size() == k) {
+                    count++;
+                }
+                freq[s[i]]--;
+                if (freq[s[i]] == 0) freq.erase(s[i]); // remove right element compleltely
+                i++;
             }
             j++;
         }
