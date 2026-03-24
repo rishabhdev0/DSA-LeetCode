964b560195d4f120a8e84ad57fddc81d8e1eeb6b
Rishabh pandey <rishabh52003@gmail.com>
2026-03-24 11:52:28 +0530

    Time: 21 ms (96.46%) | Memory: 104.9 MB (98.98%) - LeetSync

--- 3958-minimum-removals-to-balance-array/minimum-removals-to-balance-array.cpp
+++ 3958-minimum-removals-to-balance-array/minimum-removals-to-balance-array.cpp
@@ -1,22 +1,18 @@
 class Solution {
 public:
     int minRemoval(vector<int>& nums, int k) {
-        if (nums.empty()) return 0;
-        sort(nums.begin(), nums.end());
-        int n = nums.size();
-        int max_size = 0; 
-        int i = 0;
-        int j = 0;
-        while(j < n){
-            while ((long long)nums[j] > (long long)k * nums[i]) {
-                i++;
-            }
-            j++;
-            max_size = max(max_size, j - i + 1);
-        }
-        
-        return n - max_size + 1;
+        int n= nums.size();
+        sort(nums.begin() , nums.end());
+       int max_removal  = 0;
+       int i = 0 ;
+       int j  = 0;
+       while(j < n){
+          while((long long)nums[j] > (long long)k *  nums[i]){
+            i++;
+          }
+          j++;
+          max_removal = max(max_removal , j  - i +  1);
+       }
+       return n - max_removal + 1;
     }
 };
-
-auto init = atexit( [](){ ofstream("display_runtime.txt") <<'0'; });
