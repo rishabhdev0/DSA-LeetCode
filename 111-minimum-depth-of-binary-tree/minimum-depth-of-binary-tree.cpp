1055331f51ae2787b965a3a7e7f6058d9429f469
Rishabh pandey <rishabh52003@gmail.com>
2026-03-26 12:25:22 +0530

    Time: 12 ms (5.10%) | Memory: 147.1 MB (5.39%) - LeetSync

--- 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.cpp
+++ 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.cpp
@@ -12,29 +12,38 @@
 class Solution {
 public:
     int minDepth(TreeNode* root) {
-      //using bfs
-      if(root==nullptr) return 0;
-      queue<TreeNode*> que;
-      que.push(root);
-      int depth = 1;
-      while(!que.empty()){
-        int n = que.size();
-        while(n--){
-            TreeNode* temp = que.front();
-            que.pop();
-            if(temp->left  == nullptr && temp->right==nullptr){
-                return depth;
-            }
-            if(temp->left != nullptr ){
-                que.push(temp->left);
-            }
-            if(temp->right !=  nullptr){
-                que.push(temp->right);
-            }
-        }
-        depth++;
-      }
-    return depth;
+    //   //using bfs
+    //   if(root==nullptr) return 0;
+    //   queue<TreeNode*> que;
+    //   que.push(root);
+    //   int depth = 1;
+    //   while(!que.empty()){
+    //     int n = que.size();
+    //     while(n--){
+    //         TreeNode* temp = que.front();
+    //         que.pop();
+    //         if(temp->left  == nullptr && temp->right==nullptr){
+    //             return depth;
+    //         }
+    //         if(temp->left != nullptr ){
+    //             que.push(temp->left);
+    //         }
+    //         if(temp->right !=  nullptr){
+    //             que.push(temp->right);
+    //         }
+    //     }
+    //     depth++;
+    //   }
+    // return depth;
+    if (root == nullptr) return 0;
+
+    int leftDepth = minDepth(root->left);
+    int rightDepth = minDepth(root->right);
+
+    if (root->left == nullptr || root->right == nullptr) //if one subtree is there
+        return 1 + max(leftDepth, rightDepth); // then we will do  1 + max();
+
+    return 1 + min(leftDepth, rightDepth);
 
     }
 };
