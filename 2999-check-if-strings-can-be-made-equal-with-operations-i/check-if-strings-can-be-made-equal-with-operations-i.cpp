c27622db87954c6717d47bad43a8013a628f5f50
Rishabh pandey <rishabh52003@gmail.com>
2026-03-29 09:47:35 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (49.03%) - LeetSync

--- 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.cpp
+++ 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     bool canBeEqual(string s1, string s2) {
         int n = s1.length();
-        vector<bool> used(n, false);
+        // vector<bool> used(n, false);
 
         for(int i = 0; i < n; i++) {
             bool found = false;
 
             for(int j = 0; j < n; j++) {
-                if(!used[j] && s1[i] == s2[j]) {
+                if(  s1[i] == s2[j]) {
                     // same parity instead of abs(i-j)==2
                     if(i % 2 == j % 2) {
-                        used[j] = true;
+                         // used[j] = true;
                         found = true;
                         break;
                     }
