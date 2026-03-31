60362410b96498ab4905bb96a1a721f2b8119abc
Rishabh pandey <rishabh52003@gmail.com>
2026-03-31 11:50:53 +0530

    Time: 6 ms (63.71%) | Memory: 16.7 MB (38.73%) - LeetSync

--- 1105-uncrossed-lines/uncrossed-lines.cpp
+++ 1105-uncrossed-lines/uncrossed-lines.cpp
@@ -1,26 +1,51 @@
+// class Solution {
+// public:
+//     int dp[501][501];
+
+//     int solve(vector<int>& nums1 , vector<int>& nums2 , int i , int j){
+//        if(i >= nums1.size() || j >= nums2.size()) return 0;
+
+//        if(dp[i][j] != -1){
+//            return dp[i][j];
+//        }
+
+//        if(nums1[i] == nums2[j]){
+//            return dp[i][j] = 1 + solve(nums1 , nums2 , i + 1 , j + 1);
+//        }else{
+//            return dp[i][j] = max(
+//                solve(nums1 , nums2 , i + 1 , j),
+//                solve(nums1 , nums2 , i , j + 1)
+//            );
+//        }
+//     }
+
+//     int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
+//         memset(dp , -1 , sizeof(dp));
+//         return solve(nums1 , nums2 , 0 , 0);
+//     }
+// };
+
+
+// bottom up taabulation 
+
 class Solution {
 public:
-    int dp[501][501];
-
-    int solve(vector<int>& nums1 , vector<int>& nums2 , int i , int j){
-       if(i >= nums1.size() || j >= nums2.size()) return 0;
-
-       if(dp[i][j] != -1){
-           return dp[i][j];
-       }
-
-       if(nums1[i] == nums2[j]){
-           return dp[i][j] = 1 + solve(nums1 , nums2 , i + 1 , j + 1);
-       }else{
-           return dp[i][j] = max(
-               solve(nums1 , nums2 , i + 1 , j),
-               solve(nums1 , nums2 , i , j + 1)
-           );
-       }
-    }
-
     int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
-        memset(dp , -1 , sizeof(dp));
-        return solve(nums1 , nums2 , 0 , 0);
+        int n = nums1.size();
+        int m = nums2.size();
+
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= m; j++){
+                if(nums1[i - 1] == nums2[j - 1]){
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return dp[n][m];
     }
 };
