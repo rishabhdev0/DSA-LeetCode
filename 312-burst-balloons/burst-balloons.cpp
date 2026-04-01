939be0788c154490cc1f436de400649fd29b5ed5
Rishabh pandey <rishabh52003@gmail.com>
2026-04-01 10:59:01 +0530

    Time: 107 ms (60.81%) | Memory: 14 MB (28.57%) - LeetSync

--- 312-burst-balloons/burst-balloons.cpp
+++ 312-burst-balloons/burst-balloons.cpp
@@ -1,25 +1,43 @@
+// class Solution {
+// public:
+//     int dp[305][305];
+
+//     int solve(vector<int>& nums, int i, int j) {
+//         if (i > j) return 0;
+
+//         if (dp[i][j] != -1) return dp[i][j];
+
+//         int max_score = 0;
+
+//         for (int k = i; k <= j; k++) {
+//             int coins = nums[i - 1] * nums[k] * nums[j + 1]
+//                       + solve(nums, i, k - 1)
+//                       + solve(nums, k + 1, j);
+
+//             max_score = max(max_score, coins);
+//         }
+
+//         return dp[i][j] = max_score;
+//     }
+
+//     int maxCoins(vector<int>& nums) {
+//         int n = nums.size();
+
+//         // Add 1 at both ends
+//         nums.insert(nums.begin(), 1);
+//         nums.push_back(1);
+
+//         memset(dp, -1, sizeof(dp));
+
+//         return solve(nums, 1, n);
+//     }
+// };
+
+
+// bottom up -> tabulation 
+
 class Solution {
 public:
-    int dp[305][305];
-
-    int solve(vector<int>& nums, int i, int j) {
-        if (i > j) return 0;
-
-        if (dp[i][j] != -1) return dp[i][j];
-
-        int max_score = 0;
-
-        for (int k = i; k <= j; k++) {
-            int coins = nums[i - 1] * nums[k] * nums[j + 1]
-                      + solve(nums, i, k - 1)
-                      + solve(nums, k + 1, j);
-
-            max_score = max(max_score, coins);
-        }
-
-        return dp[i][j] = max_score;
-    }
-
     int maxCoins(vector<int>& nums) {
         int n = nums.size();
 
@@ -27,8 +45,22 @@ public:
         nums.insert(nums.begin(), 1);
         nums.push_back(1);
 
-        memset(dp, -1, sizeof(dp));
+        vector<vector<int>> dp(n + 2, vector<int>(n + 2, 0));
 
-        return solve(nums, 1, n);
+        // i from back → front
+        for (int i = n; i >= 1; i--) {
+            for (int j = i; j <= n; j++) { // and j from front -> back
+
+               for (int k = i; k <= j; k++) { // checking the every position which ballon on last will give the best result;
+                    int coins = nums[i - 1] * nums[k] * nums[j + 1]
+                              + dp[i][k - 1]
+                              + dp[k + 1][j];
+
+                    dp[i][j] = max(dp[i][j], coins);
+                }
+            }
+        }
+
+        return dp[1][n];
     }
 };
