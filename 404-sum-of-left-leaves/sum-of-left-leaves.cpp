fde3667aed9e8dd22aa78ae67b22295c584f6624
Rishabh pandey <rishabh52003@gmail.com>
2026-04-01 11:52:46 +0530

    Time: 0 ms (100.00%) | Memory: 16.2 MB (58.78%) - LeetSync

--- 404-sum-of-left-leaves/sum-of-left-leaves.cpp
+++ 404-sum-of-left-leaves/sum-of-left-leaves.cpp
@@ -1,17 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    int sumOfLeftLeaves(TreeNode* root) {
-        if (!root) return 0;
+    int dfs(TreeNode* root){
+        if(!root) return 0;
 
         int sum = 0;
 
-        if (root->left && !root->left->left && !root->left->right) { // root-> left exist and not its an leaf node
+        // check if left child is a leaf
+        if(root->left && !root->left->left && !root->left->right){
             sum += root->left->val;
         }
 
-        sum += sumOfLeftLeaves(root->left);
-        sum += sumOfLeftLeaves(root->right);
+        sum += dfs(root->left);
+        sum += dfs(root->right);
 
         return sum;
+    }
+
+    int sumOfLeftLeaves(TreeNode* root) {
+        return dfs(root);  // ✅ FIXED
     }
 };
