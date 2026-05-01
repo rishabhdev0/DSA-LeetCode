855d55d778160312af61cb2348de063b013ac4b1
Rishabh pandey <rishabh52003@gmail.com>
2026-05-01 19:46:36 +0530

    Time: 7 ms (18.71%) | Memory: 9.5 MB (99.22%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,15 +1,8 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if (s.length() != t.length()) return false;
-        vector<int>freq(26 , 0);
-        for(char ch : s){
-            freq[ch-'a']++;
-        }
-       for(char ch : t){
-        freq[ch-'a']--;
-        if(freq[ch-'a'] < 0) return false;
-       }
-       return true;
+        sort(s.begin() , s.end());
+        sort(t.begin() , t.end());
+        return s ==t;
     }
 };
