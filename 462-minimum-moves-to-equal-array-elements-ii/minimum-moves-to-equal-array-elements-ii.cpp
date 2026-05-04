76e3cb8e9e1413db76a02131c01fc4e4e31ed6d6
Rishabh pandey <rishabh52003@gmail.com>
2026-05-04 23:31:41 +0530

    Time: 3 ms (27.23%) | Memory: 15.6 MB (80.34%) - LeetSync

---
+++ 462-minimum-moves-to-equal-array-elements-ii/minimum-moves-to-equal-array-elements-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minMoves2(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        
+        int median = nums[n / 2];  // works for both even & odd
+        
+        int count = 0;
+        for(int i = 0; i < n; i++){
+            count += abs(nums[i] - median);
+        }
+        
+        return count;
+    }
+};
