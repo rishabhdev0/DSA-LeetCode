cbf486c8078f902688388ca30ade78d38e9f501c
Rishabh pandey <rishabh52003@gmail.com>
2026-05-08 23:16:42 +0530

    Time: 48 ms (54.66%) | Memory: 28.9 MB (91.62%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,39 +1,26 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        vector<vector<int>> result;
         int n = nums.size();
-        if (n < 3) return result;
-
-        for (int i = 0; i < n - 2; i++) {
-            // Skip duplicate elements for i
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-
-            int start = i + 1;
-            int end = n - 1;
-
-            while (start < end) {
-                int sum = nums[i] + nums[start] + nums[end];
-
-                if (sum == 0) {
-                    result.push_back({nums[i], nums[start], nums[end]});
-
-                    // Skip duplicates for start and end
-                    while (start < end && nums[start] == nums[start + 1]) start++;
-                    while (start < end && nums[end] == nums[end - 1]) end--;
-
-                    start++;
-                    end--;
-                } 
-                else if (sum < 0) {
-                    start++;
-                } 
-                else {
-                    end--;
-                }
-            }
+        sort(nums.begin() , nums.end());
+        vector<vector<int>>result;
+        for(int i = 0 ; i < n - 2 ; i++){
+           if(i > 0 && nums[i] == nums[i-1]) continue;
+             int start = i + 1;
+             int end = n - 1;
+             while(start < end){
+                 int sum = nums[i] + nums[start] + nums[end];
+                 if(sum == 0){
+                    result.push_back({nums[i] , nums[start] , nums[end]});
+                 while(start < end && nums[start] == nums[start + 1]) start++;
+                 while(start < end && nums[end] == nums[end - 1]) end--;
+                 start++;
+                 end--;
+             }else if(sum > 0){
+                end--;
+             }else start++;
         }
+    }
         return result;
     }
 };
