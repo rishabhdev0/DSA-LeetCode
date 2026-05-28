de0ed77432d1dec00ad77e827690ae7d714cbf6b
Rishabh pandey <rishabh52003@gmail.com>
2026-05-28 19:16:34 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (4.52%) - LeetSync

--- 278-first-bad-version/first-bad-version.cpp
+++ 278-first-bad-version/first-bad-version.cpp
@@ -6,16 +6,16 @@ public:
     int firstBadVersion(int n) {
         int start = 1;
         int end = n;
-        int ans = -1;
-        while (start <= end) {
+        int badVer = -1;
+        while(start <= end){
             int mid = start + (end - start) / 2;
-            if (isBadVersion(mid)) {
-                ans = mid;        
-                end = mid - 1;     
-            } else {
-                start = mid + 1;   
+            if(isBadVersion(mid)){
+                badVer = mid;
+                end = mid - 1;
+            }else {
+                start = mid + 1;
             }
         }
-        return ans;
+        return badVer;
     }
 };
