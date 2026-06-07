61a30ab84c68913223818e23fe53e78d99b7dc66
Rishabh pandey <rishabh52003@gmail.com>
2026-06-07 13:46:16 +0530

    Time: 1 ms (77.06%) | Memory: 102.4 MB (37.57%) - LeetSync

--- 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.cpp
+++ 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.cpp
@@ -2,26 +2,32 @@ class Solution {
 public:
     int minOperations(vector<int>& nums, int x) {
         int n = nums.size();
-        long long total = accumulate(nums.begin() , nums.end() , 0LL);
-        int target =  total - x;
-        if(target < 0) return -1;
-        if(target == 0) return n;
+        int total_sum = accumulate(nums.begin(), nums.end(), 0);
 
-        int curr_sum = 0;
-        int min_len = -1;
+        int target = total_sum - x;
+
+        if (target < 0) return -1;
+        if (target == 0) return n;
+
         int i = 0;
-        int j = 0;
-        while(j < n){
-            curr_sum += nums[j];
-            while(curr_sum > target){
-                curr_sum -= nums[i];
+        int sum = 0;
+        int maxLen = -1;
+
+        for (int j = 0; j < n; j++) {
+            sum += nums[j];
+
+            while (i <= j && sum > target) {
+                sum -= nums[i];
                 i++;
             }
-            if(curr_sum == target){
-                min_len = max(min_len , j - i + 1);
+
+            if (sum == target) {
+                maxLen = max(maxLen, j - i + 1);
             }
-            j++;
         }
-        return min_len == -1 ? -1 : n - min_len;
+
+        if (maxLen == -1) return -1;
+
+        return n - maxLen;
     }
 };
