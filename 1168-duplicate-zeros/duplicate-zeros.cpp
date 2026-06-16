3506a41d4d7a82fc677d69aebc9b04e54ddce448
Rishabh pandey <rishabh52003@gmail.com>
2026-06-16 11:56:51 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (47.43%) - LeetSync

--- 1168-duplicate-zeros/duplicate-zeros.cpp
+++ 1168-duplicate-zeros/duplicate-zeros.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
     void duplicateZeros(vector<int>& arr) {
-        int n=arr.size();
-        int count_zeros=0;
-        for(int i=0;i<n;i++){
-            if(arr[i]==0){
-                count_zeros++;
+        int n = arr.size();
+        int zeroCount = 0;
+        for(int i = 0 ; i < n ; i++){
+            if(arr[i] == 0){
+                zeroCount++;
             }
         }
-
-        for(int i=n-1;i>=0;i--){
-            if(i+count_zeros<n){
-                arr[i+count_zeros]=arr[i];
+        for(int i = n - 1 ; i >= 0 ; i--){
+            if(i + zeroCount < n){
+                arr[i + zeroCount] = arr[i];
             }
-            if(arr[i]==0){
-                count_zeros--;
-                if(i+count_zeros<n){
-                    arr[i+count_zeros]=0;
+            if(arr[i] == 0){
+                zeroCount--;
+                if(i + zeroCount < n){
+                    arr[i + zeroCount] = 0;
                 }
             }
-        }    
+        }
     }
 };
