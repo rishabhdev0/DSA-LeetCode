6fbd5124c152893cc88d095b8865de34eb5b60f0
Rishabh pandey <rishabh52003@gmail.com>
2026-06-24 13:56:41 +0530

    Time: 33 ms (58.17%) | Memory: 29.9 MB (44.80%) - LeetSync

--- 491-non-decreasing-subsequences/non-decreasing-subsequences.cpp
+++ 491-non-decreasing-subsequences/non-decreasing-subsequences.cpp
@@ -1,31 +1,28 @@
 class Solution {
 public:
     vector<vector<int>> result;
-    vector<int> curr;
-    
-    void backtracking(vector<int>& nums, int idx) {
-        if(curr.size() >= 2) {
-            result.push_back(curr);
+    vector<int> temp;
+
+    void backtracking(vector<int>& nums, int start) {
+        if (temp.size() >= 2) {
+            result.push_back(temp);
         }
-        
-        unordered_set<int> used;  
-        
-        for(int i = idx; i < nums.size(); i++) {
-            if(used.find(nums[i]) != used.end()) {
-                continue;
-            }
-            
-            if(curr.empty() || nums[i] >= curr.back()) {
-                used.insert(nums[i]); 
-                curr.push_back(nums[i]);
-                backtracking(nums, i + 1); 
-                curr.pop_back();
-             // we don't remove from used set here because we need to prevent duplicates
-             // at the same level across different branches
+
+        unordered_set<int> used;
+
+        for (int i = start; i < nums.size(); i++) {
+            if (used.count(nums[i])) continue;
+
+            if (temp.empty() || nums[i] >= temp.back()) {
+                used.insert(nums[i]);
+
+                temp.push_back(nums[i]);
+                backtracking(nums, i + 1);
+                temp.pop_back();
             }
         }
     }
-    
+
     vector<vector<int>> findSubsequences(vector<int>& nums) {
         backtracking(nums, 0);
         return result;
