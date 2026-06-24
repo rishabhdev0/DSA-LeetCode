6f2e8bde0d4a7747530ed0518f31bdb8d010b2a1
Rishabh pandey <rishabh52003@gmail.com>
2026-06-24 11:28:18 +0530

    Time: 775 ms (16.62%) | Memory: 11.8 MB (76.24%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,61 +1,20 @@
-// class Solution {
-// public:
-//     int dp[21][2001];
-//     int totalSum;
-
-//     int solve(vector<int>& nums, int target, int currSum, int idx) {
-//         if (idx == nums.size()) {
-//            if (currSum == target) return 1;
-//             else return 0;
-//         }
-
-//         if (dp[idx][currSum + totalSum] != -1) {
-//             return dp[idx][currSum + totalSum];
-//         }
-
-//         int plus = solve(nums, target, currSum + nums[idx], idx + 1);
-//         int minus = solve(nums, target, currSum - nums[idx], idx + 1);
-
-//         return dp[idx][currSum + totalSum] = plus + minus;
-//     }
-
-//     int findTargetSumWays(vector<int>& nums, int target) {
-//         totalSum = accumulate(nums.begin(), nums.end(), 0);
-
-//         // if (abs(target) > totalSum) return 0;
-
-//         memset(dp, -1, sizeof(dp));
-//         return solve(nums, target, 0, 0);
-//     }
-// };
-
-// tabulation -> bottom up approach
-
 class Solution {
 public:
-    int findTargetSumWays(vector<int>& nums, int target) {
-        int n = nums.size();
-        int totalSum = 0;
-        
-        for(int num : nums) {
-            totalSum += num;
-        }
-
-        // Edge cases
-        if(abs(target) > totalSum) return 0;
-        if((target + totalSum) % 2 != 0) return 0;
-
-        int subsetSum = (target + totalSum) / 2;
-
-        vector<int> dp(subsetSum + 1, 0);
-        dp[0] = 1;
-
-        for(int num : nums) {
-            for(int j = subsetSum; j >= num; j--) {
-                dp[j] += dp[j - num];
+     int count = 0;
+     void backtracking(vector<int>& nums , int target , int idx){
+         if(idx == nums.size()){
+             if(target == 0){
+             count++;
             }
-        }
+            return;
+         }
 
-        return dp[subsetSum];
+          backtracking(nums , target + nums[idx] , idx + 1);
+          backtracking(nums , target - nums[idx] , idx + 1);
+     }
+    int findTargetSumWays(vector<int>& nums, int target) {
+        // i will solve it using backtracking only , it will be TLE but fine;
+         backtracking(nums , target , 0);
+         return count;
     }
 };
