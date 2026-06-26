bbbc72ed17a0a5db2a15a13e76ba6252852885c4
Rishabh pandey <rishabh52003@gmail.com>
2026-06-26 14:06:48 +0530

    Time: 3 ms (76.23%) | Memory: 7.9 MB (71.97%) - LeetSync

--- 1160-letter-tile-possibilities/letter-tile-possibilities.cpp
+++ 1160-letter-tile-possibilities/letter-tile-possibilities.cpp
@@ -1,22 +1,29 @@
 class Solution {
 public:
-int count;
-void backtracking(string tiles , vector<bool>& used){
-    count++;
-    for(int i = 0 ; i < tiles.size() ; i++){
-        if(used[i] || (i > 0 && tiles[i] == tiles[i-1] && !used[i-1])){
-            continue;
+    void backtracking(string& tiles, vector<bool>& used, int& count) {
+        for (int i = 0; i < tiles.size(); i++) {
+
+            if (used[i]) continue;
+
+            if (i > 0 && tiles[i] == tiles[i - 1] && !used[i - 1]) continue;
+
+            used[i] = true;
+            count++;  
+
+            backtracking(tiles, used, count);
+
+            used[i] = false;
         }
-        used[i] = true;
-        backtracking(tiles , used);
-        used[i] = false;
     }
-}
+
     int numTilePossibilities(string tiles) {
-        sort(tiles.begin() , tiles.end());
-        vector<bool>used(tiles.size() , false);
-        count = -1;
-        backtracking(tiles , used);
+        sort(tiles.begin(), tiles.end());
+
+        int count = 0;
+        vector<bool> used(tiles.size(), false);
+
+        backtracking(tiles, used, count);
+
         return count;
     }
 };
