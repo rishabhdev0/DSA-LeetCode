3f1a24b8a1fb455c99576eddaec008e09d5c3929
Rishabh pandey <rishabh52003@gmail.com>
2026-06-26 12:54:50 +0530

    Time: 96 ms (34.39%) | Memory: 165.1 MB (42.91%) - LeetSync

--- 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.cpp
+++ 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.cpp
@@ -1,45 +1,45 @@
 class Solution {
 public:
-    typedef pair<int, pair<int, int>> p; // sum -> i, j
-    
+    typedef pair<int, pair<int, int>> p;
+
     vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
         int n = nums1.size();
         int m = nums2.size();
+
         vector<vector<int>> result;
+        if (n == 0 || m == 0 || k == 0) return result;
+
         priority_queue<p, vector<p>, greater<p>> pq;
-    
         set<pair<int, int>> visited;
-        
+
         int sum = nums1[0] + nums2[0];
+
         pq.push({sum, {0, 0}});
         visited.insert({0, 0});
-        
+
         while (k > 0 && !pq.empty()) {
-            auto current = pq.top();
+            auto top = pq.top();
             pq.pop();
-            
-            int i = current.second.first;
-            int j = current.second.second;
-            
-            // Add current pair to result
+
+            int i = top.second.first;
+            int j = top.second.second;
+
             result.push_back({nums1[i], nums2[j]});
             k--;
-            
-            // Push next possible pairs
+
             if (i + 1 < n && visited.find({i + 1, j}) == visited.end()) {
-                int sum1 = nums1[i + 1] + nums2[j];
-                pq.push({sum1, {i + 1, j}});
+                int sum2 = nums1[i + 1] + nums2[j];
+                pq.push({sum2, {i + 1, j}});
                 visited.insert({i + 1, j});
             }
-            
+
             if (j + 1 < m && visited.find({i, j + 1}) == visited.end()) {
-                int sum2 = nums1[i] + nums2[j + 1];
-                pq.push({sum2, {i, j + 1}});
+                int sum3 = nums1[i] + nums2[j + 1];
+                pq.push({sum3, {i, j + 1}});
                 visited.insert({i, j + 1});
             }
         }
-        
+
         return result;
     }
 };
-
