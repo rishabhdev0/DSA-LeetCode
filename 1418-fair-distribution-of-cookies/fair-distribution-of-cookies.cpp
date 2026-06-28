a3a57b5e00d2f56590f478019173eeb4162cfcf5
Rishabh pandey <rishabh52003@gmail.com>
2026-06-28 09:20:17 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (76.07%) - LeetSync

--- 1418-fair-distribution-of-cookies/fair-distribution-of-cookies.cpp
+++ 1418-fair-distribution-of-cookies/fair-distribution-of-cookies.cpp
@@ -1,29 +1,32 @@
 class Solution {
 public:
     int result = INT_MAX;
-    int n;
 
-    void backtrack(vector<int>& cookies, vector<int>& children, int k, int idx) {
-        if (idx >= n) {
+    void backtracking(vector<int>& cookies, vector<int>& children, int k, int idx) {
+        if(idx >= cookies.size()) {
             int unfairness = *max_element(begin(children), end(children));
             result = min(result, unfairness);
             return;
         }
 
-        int curr_cookies = cookies[idx];
+        int curr_Cookies = cookies[idx];
 
-        for (int i = 0; i < k; i++) {
-            children[i] += curr_cookies;  // Give cookies[idx] to child i
-            backtrack(cookies, children, k, idx + 1); //explore
-            children[i] -= curr_cookies;  // Backtrack
-            if(children[i] ==0) break;
+        for(int i = 0; i < k; i++) {
+            children[i] += curr_Cookies;
+
+            backtracking(cookies, children, k, idx + 1);
+
+            children[i] -= curr_Cookies;
+
+            if(children[i] == 0) break;
         }
     }
 
     int distributeCookies(vector<int>& cookies, int k) {
-        n = cookies.size();
         vector<int> children(k, 0);
-        backtrack(cookies, children, k, 0);
+
+        backtracking(cookies, children, k, 0);
+
         return result;
     }
 };
