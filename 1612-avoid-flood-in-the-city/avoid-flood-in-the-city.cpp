95ed56c9f0a38699710ab7675936299f3195f3ff
Rishabh pandey <rishabh52003@gmail.com>
2026-07-03 09:41:42 +0530

    Time: 171 ms (25.80%) | Memory: 149.8 MB (48.98%) - LeetSync

--- 1612-avoid-flood-in-the-city/avoid-flood-in-the-city.cpp
+++ 1612-avoid-flood-in-the-city/avoid-flood-in-the-city.cpp
@@ -2,30 +2,33 @@ class Solution {
 public:
     vector<int> avoidFlood(vector<int>& rains) {
         int n = rains.size();
-        vector<int>result(n , -1);
-        unordered_map<int , int>LastRains; // rain , the last day it rained
-        set<int>Dry; // to check if i dry the lake;
+        vector<int> result(n, -1);
 
-        for(int i = 0 ; i < n ; i++){
-             if(rains[i] == 0){
-                Dry.insert(i);
-                result[i] = 1; // by default 1 , will change later;
-             }else {
-                // not a dry lake
+        unordered_map<int, int> lastRain; // lake -> last rainy day
+        set<int> dry; // dry days in sorted order
+
+        for (int i = 0; i < n; i++) {
+            if (rains[i] == 0) {
+                dry.insert(i);
+                result[i] = 1;
+            } else {
                 int lake = rains[i];
-                if(LastRains.count(lake)){
-                    // lake found before , time to find a dry one
-                    auto it = Dry.upper_bound(LastRains[lake]);
-                    if(it == Dry.end()) return {};
-                    int DryDay = *it;
-                    result[DryDay] = lake;
-                    Dry.erase(DryDay);
+
+                if (lastRain.count(lake)) {
+                    auto it = dry.upper_bound(lastRain[lake]);
+
+                    if (it == dry.end()) {
+                        return {};
+                    }
+
+                    int dryDay = *it;
+                    result[dryDay] = lake;
+                    dry.erase(it);
                 }
-                LastRains[lake] = i;
-             }
+
+                lastRain[lake] = i;
+            }
         }
         return result;
     }
 };
-
-auto init = atexit([]() { ofstream("display_runtime.txt") << "0"; });
