6414308624b7974e5a26a225c6ba287804424142
Rishabh pandey <rishabh52003@gmail.com>
2026-07-05 14:23:51 +0530

    Time: 83 ms (88.90%) | Memory: 132.1 MB (79.56%) - LeetSync

--- 1962-single-threaded-cpu/single-threaded-cpu.cpp
+++ 1962-single-threaded-cpu/single-threaded-cpu.cpp
@@ -2,44 +2,42 @@ class Solution {
 public:
     vector<int> getOrder(vector<vector<int>>& tasks) {
         int n = tasks.size();
-        vector<array<int, 3>> SortedTask; // {enqueue_time, processing_time, index}
+
+        vector<array<int, 3>> sortedTask;
 
         for (int i = 0; i < n; i++) {
-            SortedTask.push_back({tasks[i][0], tasks[i][1], i});
+            sortedTask.push_back({tasks[i][0], tasks[i][1], i});
         }
 
-        // Sort by enqueue_time
-        sort(SortedTask.begin(), SortedTask.end());
+        sort(sortedTask.begin(), sortedTask.end());
 
+        priority_queue<
+            pair<int, int>,
+            vector<pair<int, int>>,
+            greater<pair<int, int>>
+        > pq;
+
+        long long currTime = 0;
+        int i = 0;
         vector<int> result;
-        long long curr_time = 0;
-        int index = 0;
 
-        // Min-heap: {processing_time, index}
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-
-        while (index < n || !pq.empty()) {
-            // If CPU is idle, jump to the next available task
-            if (pq.empty() && curr_time < SortedTask[index][0]) {
-                curr_time = SortedTask[index][0];
+        while (i < n || !pq.empty()) {
+            if (pq.empty() && currTime < sortedTask[i][0]) {
+                currTime = sortedTask[i][0];
             }
 
-            // Push all tasks that can be started now
-            while (index < n && SortedTask[index][0] <= curr_time) {
-                pq.push({SortedTask[index][1], SortedTask[index][2]});
-                index++;
+            while (i < n && sortedTask[i][0] <= currTime) {
+                pq.push({sortedTask[i][1], sortedTask[i][2]});
+                i++;
             }
 
-            // Pick the task with smallest processing time (and smallest index if tie)
-            auto curr_task = pq.top();
+            auto top = pq.top();
             pq.pop();
 
-            curr_time += curr_task.first; // add processing time
-            result.push_back(curr_task.second);
+            currTime += top.first;
+            result.push_back(top.second);
         }
 
         return result;
     }
 };
-
-auto init = atexit([]() { ofstream("display_runtime.txt") << "0"; });
