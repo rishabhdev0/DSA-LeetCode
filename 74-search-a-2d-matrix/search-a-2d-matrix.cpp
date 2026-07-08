437398b949c7655971d177bcbb7c6ea2a9bc8de1
Rishabh pandey <rishabh52003@gmail.com>
2026-07-08 20:40:34 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (44.92%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -4,13 +4,13 @@ public:
         int row = matrix.size();
         int col = matrix[0].size();
         int start = 0;
-        int end = row* col - 1;
+        int end = row * col - 1;
         while(start <= end){
-            int mid = start + (end - start)/2;
+            int mid = start + (end - start) / 2;
             int index = matrix[mid / col][mid % col];
             if(index == target) return true;
             else if(index < target) start = mid + 1;
-            else end = mid - 1;
+            else end = mid - 1; 
         }
         return false;
     }
