8adb1832a2e5b226dc54ac1e0fb908c829ee63ef
Rishabh pandey <rishabh52003@gmail.com>
2026-07-12 13:57:57 +0530

    Time: 80 ms (5.02%) | Memory: 36.7 MB (5.18%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,34 +1,53 @@
 class Solution {
-private:
-    vector<vector<int>> dir = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}}; // Fixed directions
-    
-    void dfs(vector<vector<char>>& grid, int i, int j) {
-        int n = grid.size();
-        int m = grid[0].size();
-        if (i < 0 || i >= n || j < 0 || j >= m || grid[i][j] != '1') return;
-        grid[i][j] = '$';
-        // Explore all four directions
-        for (auto& d : dir) {
-            int new_i = i + d[0];
-            int new_j = j + d[1];
-            dfs(grid, new_i, new_j);
-        }
-    }
-
 public:
+    vector<vector<int>> directions = {
+        {1, 0},
+        {-1, 0},
+        {0, 1},
+        {0, -1}
+    };
+
     int numIslands(vector<vector<char>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        int island = 0;
-        
+
+        int islands = 0;
+
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
+
                 if (grid[i][j] == '1') {
-                    dfs(grid, i, j);
-                    island++;
+                    queue<pair<int, int>> que;
+
+                    que.push({i, j});
+                    grid[i][j] = '0';
+
+                    while (!que.empty()) {
+                        auto [row, col] = que.front();
+                        que.pop();
+
+                        for (auto dir : directions) {
+                            int newRow = row + dir[0];
+                            int newCol = col + dir[1];
+
+                            if (
+                                newRow >= 0 &&
+                                newCol >= 0 &&
+                                newRow < n &&
+                                newCol < m &&
+                                grid[newRow][newCol] == '1'
+                            ) {
+                                grid[newRow][newCol] = '0';
+                                que.push({newRow, newCol});
+                            }
+                        }
+                    }
+
+                    islands++;
                 }
             }
         }
-        return island;
+
+        return islands;
     }
 };
