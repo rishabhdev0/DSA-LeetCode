084b3c98069bc611893a7294ede0712782a19a91
Rishabh pandey <rishabh52003@gmail.com>
2026-07-15 19:35:05 +0530

    Time: 7 ms (18.24%) | Memory: 15.1 MB (15.16%) - LeetSync

--- 684-redundant-connection/redundant-connection.cpp
+++ 684-redundant-connection/redundant-connection.cpp
@@ -1,51 +1,32 @@
 class Solution {
 public:
-    vector<int> rank;
-    vector<int> parent;
-
-    int find(int x) {
-        if (x == parent[x]) return x;
-        return parent[x] = find(parent[x]);
-    }
-
-    void unionSet(int x, int y) {
-        int x_parent = find(x);
-        int y_parent = find(y);
-        if (x_parent == y_parent) return;
-
-        if (rank[x_parent] > rank[y_parent]) {
-            parent[y_parent] = x_parent;
-        } else if (rank[x_parent] < rank[y_parent]) {
-            parent[x_parent] = y_parent;
-        } else {
-            parent[y_parent] = x_parent;
-            rank[x_parent]++;
+    bool dfs(int start , int end , vector<bool> &visited , vector<vector<int>> &adj){
+        if(start == end){
+            return true;
         }
-    }
-
-    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
-        int n = edges.size();
-        rank.resize(n + 1, 0);
-        parent.resize(n + 1);
-
-        for (int i = 1; i <= n; i++) {
-            parent[i] = i;
-        }
-
-        for (auto edge : edges) {
-            int u = edge[0];
-            int v = edge[1];
-
-            int parent_u = find(u);
-            int parent_v = find(v);
-            // If same parent => cycle (redundant edge)
-            if (parent_u == parent_v) {
-                return {u, v};
-            } else {
-                unionSet(u, v);
+        visited[start] = true;
+        for(int neighbor : adj[start]){
+            if(!visited[neighbor]){
+                if(dfs(neighbor , end , visited , adj)){
+                    return true;
+                }
             }
         }
-
+        return false;
+    }
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        int n = edges.size();
+        vector<vector<int>>adj(n+1);
+        for(auto &edge : edges){
+            int u = edge[0];
+            int v = edge[1];
+            vector<bool>visited(n + 1 , false);
+            if(dfs(u , v , visited , adj)){
+                return {u , v};
+            }
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
         return {};
     }
 };
