716cbcd67a09c383646b5cd691fb69f57e72183e
Rishabh pandey <rishabh52003@gmail.com>
2026-07-17 16:14:24 +0530

    Time: 44 ms (12.47%) | Memory: 57.2 MB (11.16%) - LeetSync

--- 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
@@ -1,57 +1,42 @@
 class Solution {
 public:
-    vector<int> rank;
-    vector<int> parent;
-    
-    int find(int x) {
-        if (x == parent[x])
-            return x;
-        return parent[x] = find(parent[x]);
-    }
-    
-    void unionSet(int x, int y) { 
-        int x_parent = find(x);
-        int y_parent = find(y);
-        if (x_parent == y_parent) return;
-        if (rank[x_parent] > rank[y_parent]) {
-            parent[y_parent] = x_parent;
-        } else if (rank[x_parent] < rank[y_parent]) {
-            parent[x_parent] = y_parent;
-        } else {
-            parent[x_parent] = y_parent;
-            rank[y_parent]++;
-        }
-    }
-    
-    int makeConnected(int n, vector<vector<int>>& connections) {
-        rank.resize(n, 0);
-        parent.resize(n);
-        for (int i = 0; i < n; i++) {
-            parent[i] = i;
-        }
-        int extraCables = 0;  
-        int components = n;   
-        for (auto& connection : connections) {
-            int u = connection[0];
-            int v = connection[1];
-            
-            int parent_u = find(u);
-            int parent_v = find(v);
-            
-            if (parent_u == parent_v) {
-                // extra
-                extraCables++;
-            } else {
-                // Connect the components
-                unionSet(u, v);
-                components--;
+    void dfs(vector<vector<int>>& adj, vector<bool>& visited, int node) {
+        visited[node] = true;
+
+        for (auto neighbor : adj[node]) {
+            if (!visited[neighbor]) {
+                dfs(adj, visited, neighbor);
             }
         }
-        int neededCables = components - 1;
-        if (extraCables >= neededCables) {
-            return neededCables;
-        } else {
+    }
+
+    int makeConnected(int n, vector<vector<int>>& connections) {
+
+        if (connections.size() < n - 1) {
             return -1;
         }
+
+        vector<vector<int>> adj(n);
+
+        for (auto connect : connections) {
+            int u = connect[0];
+            int v = connect[1];
+
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+
+        int groups = 0;
+
+        vector<bool> visited(n, false);
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                dfs(adj, visited, i);
+                groups++;
+            }
+        }
+
+        return groups - 1;
     }
 };
