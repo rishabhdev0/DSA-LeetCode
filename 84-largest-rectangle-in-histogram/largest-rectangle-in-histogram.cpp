1ff9e25241c40216aac9f19457ef63262a052a21
Rishabh pandey <rishabh52003@gmail.com>
2026-07-19 02:17:10 +0530

    Time: 36 ms (15.11%) | Memory: 83.8 MB (54.56%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -2,32 +2,28 @@ class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
         int n = heights.size();
-        stack<pair<int, int>> st; // {startIndex, height}
-        int max_area = 0;
-
-        for (int i = 0; i < n; i++) {
-            int start = i;
-
-            while (!st.empty() && st.top().second > heights[i]) {
-                int index = st.top().first;
+        stack<pair<int , int>>st; //  {lowest_index till now , heights};
+        int maxArea = 0;
+        for(int  i = 0 ; i < n ; i++){
+            int currIdx = i;
+            while(!st.empty() && st.top().second > heights[i]){
                 int height = st.top().second;
+                int small_idx = st.top().first;
+
                 st.pop();
 
-                max_area = max(max_area, height * (i - index));
-                start = index;
+
+                maxArea = max(maxArea , height * (i - small_idx));
+                currIdx = small_idx;
             }
-
-            st.push({start, heights[i]});
+            st.push({currIdx , heights[i]});
         }
-
-        while (!st.empty()) {
+        while(!st.empty()){
             int index = st.top().first;
             int height = st.top().second;
             st.pop();
-
-            max_area = max(max_area, height * (n - index));
+            maxArea = max(maxArea , height * (n - index));
         }
-
-        return max_area;
+        return maxArea;
     }
 };
