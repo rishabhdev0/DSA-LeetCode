b5693ac7a8fb6d3833ca9830a4f11736be046654
Rishabh pandey <rishabh52003@gmail.com>
2026-07-21 23:24:20 +0530

    Time: 0 ms (100.00%) | Memory: 14.4 MB (61.82%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,49 +1,19 @@
-// class Solution {
-// public:
-//     int dp[2501][2501];
-
-//     int solve(vector<int>& nums, int idx, int prev, int n) {
-//         if (idx == n) return 0;
-
-//         if (dp[idx][prev + 1] != -1)
-//             return dp[idx][prev + 1];
-
-//         int take = 0;
-//         if (prev == -1 || nums[prev] < nums[idx]) {
-//             take = 1 + solve(nums, idx + 1, idx, n);
-//         }
-
-//         int skip = solve(nums, idx + 1, prev, n);
-
-//         return dp[idx][prev + 1] = max(take, skip);
-//     }
-
-//     int lengthOfLIS(vector<int>& nums) {
-//         int n = nums.size();
-//         memset(dp, -1, sizeof(dp));
-//         return solve(nums, 0, -1, n);
-//     }
-// };
-
-// bttom up -> tabulation 
-
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<int>dp(n,1);
-        int max_len = 1;
+        vector<int> dp(n , 1);
+        int max_Len = 1;
         for(int i = 0 ; i < n ; i++){
             for(int j = 0 ; j < i ; j++){
-                if(nums[j] < nums[i]){
+                if(nums[i] > nums[j]){
                     dp[i] = max(dp[i] , dp[j] + 1);
-                    max_len = max(max_len , dp[i]);
+                    max_Len = max(max_Len , dp[i]);
                 }
             }
         }
-        return max_len;
+         return max_Len;
     }
 };
 
 auto init = atexit([]() { ofstream("display_runtime.txt") << "000"; });
-
