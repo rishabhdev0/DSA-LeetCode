68acb34e1943fcbe302260a50d6c168a5fe53ff4
Rishabh pandey <rishabh52003@gmail.com>
2026-07-22 19:23:15 +0530

    Time: 104 ms (28.73%) | Memory: 89.2 MB (68.56%) - LeetSync

--- 718-maximum-length-of-repeated-subarray/maximum-length-of-repeated-subarray.cpp
+++ 718-maximum-length-of-repeated-subarray/maximum-length-of-repeated-subarray.cpp
@@ -1,35 +1,29 @@
 class Solution {
 public:
-    int dp[1001][1001];
-    int ans = 0;
-
-    int solve(vector<int>& nums1 , vector<int>& nums2 , int i , int j){
-        
-        if(i == nums1.size() || j == nums2.size()){
-            return 0;
-        }
-
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
-
-        int length = 0;
-
-        if(nums1[i] == nums2[j]){
-            length = 1 + solve(nums1 , nums2 , i + 1 , j + 1);
-            ans = max(ans , length);
-        }
-
-        
-        solve(nums1 , nums2 , i + 1 , j);
-        solve(nums1 , nums2 , i , j + 1);
-
-        return dp[i][j] = length;
-    }
-
     int findLength(vector<int>& nums1, vector<int>& nums2) {
-        memset(dp , -1 , sizeof(dp));
-        solve(nums1 , nums2 , 0 , 0);
-        return ans;
+        int n = nums1.size();
+        int m = nums2.size();
+
+        vector<vector<int>> dp(n, vector<int>(m, 0));
+        int result = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+
+                if (nums1[i] == nums2[j]) {
+                    if (i == 0 || j == 0) {
+                        dp[i][j] = 1;
+                    } else {
+                        dp[i][j] = 1 + dp[i - 1][j - 1];
+                    }
+
+                    result = max(result, dp[i][j]);
+                } else {
+                    dp[i][j] = 0;
+                }
+            }
+        }
+
+        return result;
     }
 };
