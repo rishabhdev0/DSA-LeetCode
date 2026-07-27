578dc1243141b95e2f9bff4a241ba8aca6e39791
Rishabh pandey <rishabh52003@gmail.com>
2026-07-27 23:28:40 +0530

    Time: 17 ms (14.26%) | Memory: 35.1 MB (54.25%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -1,44 +1,31 @@
 class Solution {
-private:
-    bool canShip(vector<int>& weights, int capacity, int days) {
-        int currentLoad = 0;
-        int requiredDays = 1; 
-        
-        for(int weight : weights) {
-            // If adding this weight exceeds capacity, we need a new day
-            if(currentLoad + weight > capacity) {
-                requiredDays++;
-                currentLoad = 0;
-                
-                // If we've exceeded the allowed days, return false
-                if(requiredDays > days) {
-                    return false;
-                }
-            }
-            currentLoad += weight;
-        }
-        
-        return requiredDays <= days;
-    }
-
 public:
+    bool canShip(vector<int>& weights , int days , int mid){
+       int dayCount = 1;
+       int sum = 0;
+       for(int  i = 0 ; i <  weights.size() ; i++){
+          sum += weights[i];
+          if(sum > mid){
+            dayCount++;
+            sum  = weights[i];
+
+          }
+       }
+       return dayCount <= days;
+    }
     int shipWithinDays(vector<int>& weights, int days) {
-        int low = *max_element(weights.begin(), weights.end());
-        int high = accumulate(weights.begin(), weights.end(), 0);
-        
+        int low = *max_element(weights.begin() , weights.end());
+        int high = accumulate(weights.begin() , weights.end() , 0);
         int result = -1;
-        
-        while(low <= high) {
+        while(low <= high){
             int mid = low + (high - low) / 2;
-            
-            if(canShip(weights, mid, days)) {
+            if(canShip(weights , days , mid )){
                 result = mid;
                 high = mid - 1;
-            } else {
-                low = mid + 1; // Need larger capacity
+            }else{
+                low = mid + 1;
             }
         }
-        
         return result;
     }
 };
