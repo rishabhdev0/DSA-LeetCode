f40dc888e49a3585de2fd112da75e21205ecece0
Rishabh pandey <rishabh52003@gmail.com>
2026-07-28 00:26:10 +0530

    Time: 501 ms (10.40%) | Memory: 117.8 MB (7.36%) - LeetSync

--- 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.cpp
+++ 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.cpp
@@ -1,60 +1,32 @@
 class Solution {
 public:
+    int dp[50001];
 
-    vector<int> dp;
+    int solve(vector<vector<int>>&nums  , int idx){
+        if(idx >= nums.size()) return 0;
 
-    int getNextIndex(vector<vector<int>>& arr, int l, int target){
-        int r = arr.size() - 1;
-        int result = arr.size();
-
-        while(l <= r){
-            int mid = l + (r - l) / 2;
-
-            if(arr[mid][0] >= target){
-                result = mid;
-                r = mid - 1;
-            }
-            else{
-                l = mid + 1;
-            }
-        }
-
-        return result;
-    }
-
-    int solve(vector<vector<int>>& arr, int idx){
-
-        if(idx >= arr.size())
-            return 0;
-
-        if(dp[idx] != -1)
+        if(dp[idx] != -1){
             return dp[idx];
-
-        int next = getNextIndex(arr, idx + 1, arr[idx][1]);
-
-        int take = arr[idx][2] + solve(arr, next);
-
-        int not_take = solve(arr, idx + 1);
-
-        return dp[idx] = max(take, not_take);
-    }
-
-    int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
-
-        int n = startTime.size();
-
-        vector<vector<int>> arr(n, vector<int>(3));
-
-        for(int i = 0; i < n; i++){
-            arr[i][0] = startTime[i];
-            arr[i][1] = endTime[i];
-            arr[i][2] = profit[i];
         }
+        int next = lower_bound(nums.begin() , nums.end() , vector<int>{nums[idx][1] , 0 , 0}) - 
+        nums.begin();
+       
+       int take = nums[idx][2] + solve(nums ,next);
+       int not_took = solve(nums , idx + 1);
+       
+       return dp[idx] = max(take , not_took);
 
-        sort(arr.begin(), arr.end());
-
-        dp.resize(n, -1);
-
-        return solve(arr, 0);
+    }
+    int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
+        int n = startTime.size();
+        vector<vector<int>>nums(n , vector<int>(3));
+        for(int i = 0 ; i < n ; i++){
+            nums[i][0] = startTime[i];
+            nums[i][1] = endTime[i];
+            nums[i][2] = profit[i];
+        }
+        sort(nums.begin() , nums.end());
+        memset(dp , -1 , sizeof(dp));
+        return solve(nums , 0);
     }
 };
