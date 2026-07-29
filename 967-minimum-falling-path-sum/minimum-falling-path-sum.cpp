40f9a8402b9810cbe75ae78fa018f9588fc1dabc
Rishabh pandey <rishabh52003@gmail.com>
2026-07-30 02:22:59 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (46.87%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -1,26 +1,30 @@
 class Solution {
 public:
-    int MFS(vector<vector<int>>& A) {
-        int m = A.size();
-        vector<vector<int>> t(m, vector<int>(m));
-        
-        //initialization
-        for(int col = 0; col<m; col++) {
-            t[0][col] = A[0][col];
-        }
-        
-        for(int row = 1; row < m; row++) {
-            for(int col = 0; col < m; col++) {
-                t[row][col] = A[row][col] + min({t[row-1][col], 
-                                                 t[row-1][max(0, col-1)], 
-                                                 t[row-1][min(m-1, col+1)]});
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+
+        vector<vector<int>> dp(n, vector<int>(n));
+
+        for (int j = 0; j < n; j++)
+            dp[0][j] = matrix[0][j];
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+
+                int up = dp[i - 1][j];
+
+                int leftDiag = INT_MAX;
+                if (j > 0)
+                    leftDiag = dp[i - 1][j - 1];
+
+                int rightDiag = INT_MAX;
+                if (j < n - 1)
+                    rightDiag = dp[i - 1][j + 1];
+
+                dp[i][j] = matrix[i][j] + min({up, leftDiag, rightDiag});
             }
         }
 
-        return *min_element(t[m-1].begin(), t[m-1].end());
-        
-    }
-    int minFallingPathSum(vector<vector<int>>& A) { 
-        return MFS(A);
+        return *min_element(dp[n - 1].begin(), dp[n - 1].end());
     }
 };
