a020b2fb9bdf994aba44edf37a471d2ecafadfa0
Rishabh pandey <rishabh52003@gmail.com>
2026-08-01 18:59:17 +0530

    Time: 0 ms (100.00%) | Memory: 52.3 MB (83.80%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,45 +1,19 @@
-// class Solution {
-// public:
-//     int dp[10001];
-
-//     bool solve(vector<int>& nums , int idx){
-
-//         if(idx >= nums.size()-1) 
-//             return true;
-
-//         if(dp[idx] != -1) 
-//             return dp[idx];
-
-//         int maxJump = nums[idx];
-
-//         for(int jump = 1 ; jump <= maxJump ; jump++){
-
-//             if(solve(nums , idx + jump))
-//                 return dp[idx] = true;
-//         }
-
-//         return dp[idx] = false;
-//     }
-
-//     bool canJump(vector<int>& nums) {
-
-//         memset(dp , -1 , sizeof(dp));
-//         return solve(nums , 0);
-//     }
-// };
-
-
-// easy method;
-
-
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-       int maxReach = 0;
-       for(int i = 0 ; i < nums.size() ; i++){
-          if(i > maxReach) return false;
-          maxReach = max(maxReach , i + nums[i]);
-       }
-       return true;
+        int farthest = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+
+            if (i > farthest)
+                return false;
+
+            farthest = max(farthest, i + nums[i]);
+
+            if (farthest >= nums.size() - 1)
+                return true;
+        }
+
+        return true;
     }
 };
