8b7ef1a54f9f700405014f3db381d5541f982e78
Rishabh pandey <rishabh52003@gmail.com>
2026-08-04 12:49:00 +0530

    Time: 35 ms (75.38%) | Memory: 88.9 MB (66.43%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,33 +1,31 @@
 class Solution {
 public:
-    int dp[100001][2][3];
+    int dp[100001][2][2];
 
-    int solve(vector<int>& prices , int idx , bool buy , int limit){
+    int solve(int i, int buy, int total , vector<int>& prices) {
+        if (i >= prices.size() || total == 2)
+            return 0;
 
-        if(idx >= prices.size()) return 0;
-        if(limit == 0) return 0;
+        if (dp[i][buy][total] != -1)
+            return dp[i][buy][total];
 
-        if(dp[idx][buy][limit] != -1){
-            return dp[idx][buy][limit];
-        }
-        
         int profit = 0;
 
-        if(!buy){
-            int took = -prices[idx] + solve(prices , idx + 1 , 1 , limit);
-            int not_took = solve(prices , idx + 1 , 0 , limit);
-            profit = max(took , not_took);
-        }else{
-            int took = prices[idx] + solve(prices , idx + 1 , 0 , limit - 1);
-            int not_took = solve(prices , idx + 1 , 1 , limit);
-            profit = max(took , not_took);
+        if (buy) {
+            int took = -prices[i] + solve(i + 1, 0,total, prices);
+            int skip = solve(i + 1, 1,total , prices);
+            profit = max(took, skip);
+        } else {
+            int sell = prices[i] + solve(i + 1,1 , total + 1 , prices);
+            int skip = solve(i + 1, 0, total ,prices);
+            profit = max(sell, skip);
         }
 
-        return dp[idx][buy][limit] = profit;
+        return dp[i][buy][total] = profit;
     }
 
     int maxProfit(vector<int>& prices) {
-        memset(dp , -1 , sizeof(dp));
-        return solve(prices , 0 , 0 , 2);
+        memset(dp, -1, sizeof(dp));
+        return solve(0, 1, 0 ,prices);
     }
 };
