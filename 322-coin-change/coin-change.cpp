53d7a048ac6ec5fb91126b5adf884dfad75b2057
Rishabh pandey <rishabh52003@gmail.com>
2026-08-06 23:15:30 +0530

    Time: 32 ms (56.78%) | Memory: 18.9 MB (62.54%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,49 +1,31 @@
-// class Solution {
-// public:
-//     int dp[10001][13];
-
-//     int solve(vector<int>& coins, int target, int idx) {
-//         if (target == 0) return 0;
-//         if (idx == coins.size()) return INT_MAX;
-//         if (target < 0) return INT_MAX;
-
-//         if (dp[target][idx] != -1)
-//             return dp[target][idx];
-
-//         int not_take = solve(coins, target, idx + 1);
-
-//         int take = INT_MAX;
-//         if (coins[idx] <= target) {
-//             int res = solve(coins, target - coins[idx], idx);
-//             if (res != INT_MAX)
-//                 take = 1 + res;
-//         }
-
-//         return dp[target][idx] = min(take, not_take);
-//     }
-
-//     int coinChange(vector<int>& coins, int amount) {
-//         memset(dp, -1, sizeof(dp));
-
-//         int ans = solve(coins, amount, 0);
-
-//         return (ans == INT_MAX ? -1 : ans);
-//     }
-// };
-
-// tabulation bottom up;
-
 class Solution {
 public:
-     int coinChange(vector<int>& coins, int amount) {
-        const int INF = 1e9;
-        vector<int>dp(amount + 1 , INF);
-        dp[0] = 0;
-        for(int coin : coins){
-            for(int j = coin ; j <= amount ; j++){
-                dp[j] = min(dp[j] , 1 + dp[j - coin]);
-            }
+    vector<int> dp;
+
+    int solve(vector<int>& coins, int amount) {
+        if (amount == 0)
+            return 0;
+
+        if (amount < 0)
+            return 1e9;
+
+        if (dp[amount] != -1)
+            return dp[amount];
+
+        int ans = 1e9;
+
+        for (int coin : coins) {
+            ans = min(ans, 1 + solve(coins, amount - coin));
         }
-        return (dp[amount] >= INF ? -1 : dp[amount]);
+
+        return dp[amount] = ans;
+    }
+
+    int coinChange(vector<int>& coins, int amount) {
+        dp.assign(amount + 1, -1);
+
+        int ans = solve(coins, amount);
+
+        return ans >= 1e9 ? -1 : ans;
     }
 };
