c89ab9773bd010335bcc871cb788b46c8a83c93a
Rishabh pandey <rishabh52003@gmail.com>
2026-08-08 00:00:36 +0530

    Time: 154 ms (54.98%) | Memory: 10.8 MB (99.99%) - LeetSync

--- 312-burst-balloons/burst-balloons.cpp
+++ 312-burst-balloons/burst-balloons.cpp
@@ -1,66 +1,73 @@
-// class Solution {
-// public:
-//     int dp[305][305];
+/*
+    Genius interval DP.
 
-//     int solve(vector<int>& nums, int i, int j) {
-//         if (i > j) return 0;
+    Trying to burst balloons from FIRST to LAST is difficult because
+    every burst changes the neighbors.
 
-//         if (dp[i][j] != -1) return dp[i][j];
+    Instead, think in reverse.
 
-//         int max_score = 0;
+    Assume balloon k is the LAST balloon burst in interval [i...j].
 
-//         for (int k = i; k <= j; k++) {
-//             int coins = nums[i - 1] * nums[k] * nums[j + 1]
-//                       + solve(nums, i, k - 1)
-//                       + solve(nums, k + 1, j);
+    If k is burst last:
+        - Everything left of k has already disappeared.
+        - Everything right of k has already disappeared.
+        - Therefore the only balloons adjacent to k are
+              nums[i-1] and nums[j+1].
 
-//             max_score = max(max_score, coins);
-//         }
+    These two boundaries never change while solving [i...j].
 
-//         return dp[i][j] = max_score;
-//     }
+    So the coins gained by bursting k last are
 
-//     int maxCoins(vector<int>& nums) {
-//         int n = nums.size();
+        nums[i-1] * nums[k] * nums[j+1]
 
-//         // Add 1 at both ends
-//         nums.insert(nums.begin(), 1);
-//         nums.push_back(1);
+    After fixing k as the last balloon, the remaining work splits into
+    two completely independent subproblems:
 
-//         memset(dp, -1, sizeof(dp));
+        [i ... k-1]
+        [k+1 ... j]
 
-//         return solve(nums, 1, n);
-//     }
-// };
+    This converts a dynamic-neighbor problem into a fixed-boundary problem,
+    which is exactly why interval DP works here.
 
-
-// bottom up -> tabulation 
+    Honestly, my ASS would have never solve this problem on my won.
+*/
 
 class Solution {
 public:
+    int dp[301][301];
+
+    int solve(int i, int j, vector<int>& nums) {
+        if (i > j)
+            return 0;
+
+        if (dp[i][j] != -1)
+            return dp[i][j];
+
+        int max_score = 0;
+
+        // Assume k is the LAST balloon to burst in [i...j]
+        for (int k = i; k <= j; k++) {
+
+            int coins =
+                nums[i - 1] * nums[k] * nums[j + 1]
+                + solve(i, k - 1, nums)
+                + solve(k + 1, j, nums);
+
+            max_score = max(max_score, coins);
+        }
+
+        return dp[i][j] = max_score;
+    }
+
     int maxCoins(vector<int>& nums) {
+
         int n = nums.size();
 
-        // Add 1 at both ends
         nums.insert(nums.begin(), 1);
         nums.push_back(1);
 
-        vector<vector<int>> dp(n + 2, vector<int>(n + 2, 0));
+        memset(dp, -1, sizeof(dp));
 
-        // i from back → front
-        for (int i = n; i >= 1; i--) {
-            for (int j = i; j <= n; j++) { // and j from front -> back
-
-               for (int k = i; k <= j; k++) { // checking the every position which ballon on last will give the best result;
-                    int coins = nums[i - 1] * nums[k] * nums[j + 1]
-                              + dp[i][k - 1]
-                              + dp[k + 1][j];
-
-                    dp[i][j] = max(dp[i][j], coins);
-                }
-            }
-        }
-
-        return dp[1][n];
+        return solve(1, n, nums);
     }
 };
