2b81f4f6044a374122df58b5a4c5e54d78cb8d3f
Rishabh pandey <rishabh52003@gmail.com>
2026-08-09 23:48:57 +0530

    Time: 14 ms (37.57%) | Memory: 11 MB (91.02%) - LeetSync

---
+++ 1240-stone-game-ii/stone-game-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int n;
+    int dp[2][101][101];
+
+    int Alice_max_score(vector<int>& piles , int person , int i , int m){
+        if(i >= n) return 0;
+        if(dp[person][i][m] != -1){
+            return dp[person][i][m];
+        }
+        int result = (person == 1) ? -1 : INT_MAX;
+        int stones = 0;
+        for(int x = 1 ; x <= min(2*m , n - i) ; x++){
+            stones += piles[i+x-1];
+            if(person == 1){
+                // alice;
+                result = max(result , stones + Alice_max_score(piles , 0 , i + x , max(m , x)));
+            }else{
+                result = min(result , Alice_max_score(piles , 1 , i + x , max(m,x)));
+            }
+        }
+        return dp[person][i][m] = result;
+    }
+    int stoneGameII(vector<int>& piles) {
+        memset(dp , -1 , sizeof(dp));
+        n = piles.size();
+        return Alice_max_score(piles , 1 , 0 , 1);
+    }
+};
