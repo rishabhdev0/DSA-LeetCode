5e9857412c0200fe6163f6dadcada478c16c7b68
Rishabh pandey <rishabh52003@gmail.com>
2026-08-11 13:13:52 +0530

    Time: 7 ms (65.93%) | Memory: 11.9 MB (75.23%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,20 +1,31 @@
 class Solution {
 public:
-     int count = 0;
-     void backtracking(vector<int>& nums , int target , int idx){
-         if(idx == nums.size()){
-             if(target == 0){
-             count++;
-            }
-            return;
-         }
+    int dp[21][2001];
 
-          backtracking(nums , target + nums[idx] , idx + 1);
-          backtracking(nums , target - nums[idx] , idx + 1);
-     }
+    int solve(int idx, int currSum, vector<int>& nums, int target) {
+        
+        if (idx == nums.size()) {
+            return currSum == target;
+        }
+
+        int &ans = dp[idx][currSum + 1000];
+
+        if (ans != -1) {
+            return ans;
+        }
+
+        // Choose +
+        int take = solve(idx + 1, currSum + nums[idx], nums, target);
+
+        // Choose -
+        int skip = solve(idx + 1, currSum - nums[idx], nums, target);
+
+        return ans = take + skip;
+    }
+
     int findTargetSumWays(vector<int>& nums, int target) {
-        // i will solve it using backtracking only , it will be TLE but fine;
-         backtracking(nums , target , 0);
-         return count;
+        memset(dp, -1, sizeof(dp));
+
+        return solve(0, 0, nums, target);
     }
 };
