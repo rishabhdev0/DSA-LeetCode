759c5a59d85fdaf3e72103fd889f71944a569d24
Rishabh pandey <rishabh52003@gmail.com>
2026-08-12 19:47:25 +0530

    Time: 47 ms (47.05%) | Memory: 7.9 MB (61.43%) - LeetSync

--- 526-beautiful-arrangement/beautiful-arrangement.cpp
+++ 526-beautiful-arrangement/beautiful-arrangement.cpp
@@ -1,25 +1,22 @@
 class Solution {
-private:
-    void backtracking(int n, int pos, vector<bool>& visited, int& count) {
-        if (pos > n) {
+public:
+    void backtracking(int n , int pos , vector<bool>& visited , int& count){
+        if(pos > n){
             count++;
             return;
         }
-        
-        for (int i = 1; i <= n; i++) {
-            if (!visited[i] && (i % pos == 0 || pos % i == 0)) { // not visited 
-                visited[i] = true; // mark as viisted
-                backtracking(n, pos + 1, visited, count); // look for another pos
-                visited[i] = false; // backtrack
+        for(int i = 1 ; i <= n ; i++){
+            if(!visited[i] && (i % pos == 0 || pos % i == 0)){
+                visited[i] = true;
+                backtracking(n , pos + 1 , visited , count);
+                visited[i] = false;
             }
         }
     }
-    
-public:
     int countArrangement(int n) {
         int count = 0;
-        vector<bool> visited(n + 1, false); // 1-based indexed
-        backtracking(n, 1, visited, count);
+        vector<bool>visited(n+1 , false);
+        backtracking(n , 1 , visited , count);
         return count;
     }
 };
