fb35a7538daa1ae0c3cddc718375b89307acbb66
Rishabh pandey <rishabh52003@gmail.com>
2026-08-13 13:35:04 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (13.71%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -1,45 +1,48 @@
 class Solution {
 public:
+    int n;
     int dp[101][101];
 
-    bool solve(string& s, int n, int open, int idx){
-        if(open < 0) return false;
+    bool solve(string& s, int open, int idx) {
 
-        // 🔥 Important bounds
-        if(open > n) return false;
-        if(open > n - idx) return false;
+        if(open < 0)
+            return false;
 
-        if(idx == n){
+        if(open > n - idx)
+            return false;
+
+        if(idx == n)
             return open == 0;
-        }
 
-        if(dp[idx][open] != -1){
-            return dp[idx][open];
-        }
+        if(dp[open][idx] != -1)
+            return dp[open][idx];
 
         bool check = false;
 
-        if(s[idx] == '('){
-            check |= solve(s, n, open + 1, idx + 1);
+        if(s[idx] == '(') {
+            check = solve(s, open + 1, idx + 1);
         }
-        else if(s[idx] == ')'){
-            if(open > 0)
-                check |= solve(s, n, open - 1, idx + 1);
+        else if(s[idx] == ')') {
+            check = solve(s, open - 1, idx + 1);
         }
-        else { // '#'
-            check |= solve(s, n, open + 1, idx + 1); // '('
+        else {
+            // '*' -> '('
+            check |= solve(s, open + 1, idx + 1);
 
-            if(open > 0)
-                check |= solve(s, n, open - 1, idx + 1); // ')'
+            // '*' -> ')'
+            check |= solve(s, open - 1, idx + 1);
 
-            check |= solve(s, n, open, idx + 1); // empty
+            // '*' -> empty
+            check |= solve(s, open, idx + 1);
         }
 
-        return dp[idx][open] = check;
+        return dp[open][idx] = check;
     }
 
     bool checkValidString(string s) {
+        n = s.length();
         memset(dp, -1, sizeof(dp));
-        return solve(s, s.length(), 0, 0);
+
+        return solve(s, 0, 0);
     }
 };
