180dca9d03a6b20ce7e64a7ee9bf20a8df468886
Rishabh pandey <rishabh52003@gmail.com>
2026-08-13 12:03:26 +0530

    Time: 177 ms (5.04%) | Memory: 74.9 MB (5.07%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -1,42 +1,31 @@
 class Solution {
 public:
+    vector<vector<int>>direction = {{-1 , 0} , {0 , 1} , {0 , -1} , {1 , 0}};
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int n = mat.size();
         int m = mat[0].size();
-
-        vector<pair<int,int>> dir = {{0,1}, {0,-1}, {1,0}, {-1,0}};
-
-        queue<pair<int,int>> pq;
-
-        // push all 0s
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
+        vector<vector<int>>result(n , vector<int>(m , -1));
+        queue<pair<int , int>>que;
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < m ; j++){
                 if(mat[i][j] == 0){
-                    pq.push({i, j});
-                } else {
-                    mat[i][j] = -1; // mark unvisited
+                    result[i][j] = 0;
+                    que.push({i , j});
                 }
             }
         }
-
-        // BFS
-        while(!pq.empty()){
-            auto [x, y] = pq.front();
-            pq.pop();
-
-            for(auto [dx, dy] : dir){
-                int new_x = x + dx;
-                int new_y = y + dy;
-
-                if(new_x >= 0 && new_y >= 0 && new_x < n && new_y < m 
-                   && mat[new_x][new_y] == -1){
-
-                    mat[new_x][new_y] = mat[x][y] + 1;
-                    pq.push({new_x, new_y});
-                }
-            }
+        while(!que.empty()){
+           auto[i , j] =  que.front();
+           que.pop();
+           for(auto dir : direction){
+            int new_i = i + dir[0];
+            int new_j = j +  dir[1];
+            if(new_i >= 0 && new_j >= 0 && new_i < n && new_j < m && result[new_i][new_j] == -1){
+                result[new_i][new_j] = result[i][j] + 1; 
+                que.push({new_i , new_j});
+              }
+           }
         }
-
-        return mat;
+        return result;
     }
 };
