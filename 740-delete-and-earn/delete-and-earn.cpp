a89d74002021de793319960081266b8787cf00af
Rishabh pandey <rishabh52003@gmail.com>
2026-08-14 19:49:26 +0530

    Time: 7 ms (22.37%) | Memory: 17.2 MB (33.28%) - LeetSync

--- 740-delete-and-earn/delete-and-earn.cpp
+++ 740-delete-and-earn/delete-and-earn.cpp
@@ -1,29 +1,40 @@
 class Solution {
 public:
     int deleteAndEarn(vector<int>& nums) {
-        unordered_map<int , int>freq;
-        for(int num : nums){
+        unordered_map<int, int> freq;
+
+        for (auto num : nums) {
             freq[num]++;
         }
-        vector<int>unique;
-        for(auto &it : freq){
+
+        vector<int> unique;
+
+        for (auto& it : freq) {
             unique.push_back(it.first);
         }
-        sort(unique.begin() , unique.end());
-        
+
+        sort(unique.begin(), unique.end());
+
         int n = unique.size();
-        vector<int>dp(n , 0);
+
+        vector<int> dp(n, 0);
+
         dp[0] = unique[0] * freq[unique[0]];
-        for(int i = 1 ; i < n ; i++){
-            long long val = unique[i] * freq[unique[i]];
-            if(unique[i] == unique[i-1] + 1){
-                int take = val + (i > 1 ? dp[i-2] : 0);
-                int skip = dp[i-1];
-                dp[i] = max(take , skip);
-            }else{
-                dp[i] = dp[i-1] + val;
+
+        for (int i = 1; i < n; i++) {
+            long long val = 1LL * unique[i] * freq[unique[i]];
+
+            if (unique[i] == unique[i - 1] + 1) {
+                int took = val + (i > 1 ? dp[i - 2] : 0);
+                int skip = dp[i - 1];
+
+                dp[i] = max(took, skip);
+            }
+            else {
+                dp[i] = val + dp[i - 1];
             }
         }
-        return dp[n-1];
+
+        return dp[n - 1];
     }
 };
