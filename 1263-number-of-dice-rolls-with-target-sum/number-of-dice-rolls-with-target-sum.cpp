efc99b916d92d606be77c931a187db4514a4f719
Rishabh pandey <rishabh52003@gmail.com>
2026-08-19 20:15:50 +0530

    Time: 7 ms (92.96%) | Memory: 8.1 MB (92.32%) - LeetSync

--- 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
+++ 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
@@ -1,51 +1,35 @@
-// class Solution {
-// public:
-//     int dp[31][1001];
-//     const int MOD = 1e9 + 7;
-
-//     int solve(int dice , int k , int target){
-//         if(dice == 0 && target == 0) return 1;
-//         if(dice == 0 || target < 0) return 0;
-
-//         if(dp[dice][target] != -1){
-//             return dp[dice][target];
-//         }
-
-//         long long way = 0;
-
-//         for(int i = 1 ; i <= k ; i++){
-//             way = (way + solve(dice-1 , k , target - i))%MOD;
-//         }
-//         return dp[dice][target] = way;
-//     }
-//     int numRollsToTarget(int n, int k, int target) {
-//         memset(dp , -1 , sizeof(dp));
-//          return solve(n , k , target);
-//     }
-// };
-
-
-// bottom up -> tabulation 
-
 class Solution {
 public:
-    int numRollsToTarget(int n, int k, int target) {
-         vector<vector<int>>dp(n+1 , vector<int>(target +1  , 0));
-         const int MOD = 1e9+7;
-    
-         dp[0][0] = 1;
+     int dp[31][1001];
+    int const mod = 1e9 + 7;
+     int solve(int n , int k , int target , int sum){
+        if(n == 0 && sum == target){
+            return 1;
+        }
 
-         for(int i = 1 ; i <= n  ; i++){
-            for(int j = 1 ; j <= target ; j++){
-                int way = 0;
-                for(int face = 1 ; face <= k ; face++){
-                    if(j - face >= 0){
-                        way = (way + dp[i-1][j - face]) % MOD;
-                    }
-                }
-                dp[i][j] = way;
-            }
-         } 
-         return dp[n][target];
+        if(n ==0 && sum != target){
+            return 0;
+        }
+
+        if(n == 0){
+            return 0;
+        }
+
+        if(dp[n][sum] != -1){
+            return dp[n][sum];
+        }
+
+        long long way = 0;
+
+        for(int i = 1 ; i <= k ; i++){
+            way = (way + solve(n-1 , k  , target , sum + i)) % mod;
+        }
+
+        return dp[n][sum] = way;
+
+     }
+    int numRollsToTarget(int n, int k, int target) {
+        memset(dp , -1 , sizeof(dp));
+        return solve(n , k , target , 0);   
     }
 };
