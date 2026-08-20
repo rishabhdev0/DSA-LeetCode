e445421510e5f1ab4f9af95d2df93b9f22201eaf
Rishabh pandey <rishabh52003@gmail.com>
2026-08-20 12:20:28 +0530

    Time: 15 ms (95.75%) | Memory: 10 MB (84.81%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,49 +1,33 @@
-// class Solution {
-// public:
-//     int dp[501][501];
-
-//     int solve(string &s, int i, int j) {
-//         if (i >= j) return 0;   
-
-//         if (dp[i][j] != -1)
-//             return dp[i][j];
-
-//         if (s[i] == s[j]) {
-//             dp[i][j] = solve(s, i + 1, j - 1);
-//         } else {
-//             dp[i][j] = 1 + min(solve(s , i + 1 , j) , solve(s , i , j - 1));
-//         }
-
-//         return dp[i][j];
-//     }
-
-//     int minInsertions(string s) {
-//         memset(dp, -1, sizeof(dp));
-//         return solve(s, 0, s.length() - 1);
-//     }
-// };
-
-
-// bottom up -> tabulation
-
 class Solution {
 public:
-    int minInsertions(string s) {
-        int n = s.length();
-        vector<vector<int>> dp(n, vector<int>(n, 0));
+    int dp[501][501];
 
-        for (int len = 2; len <= n; len++) {
-            for (int i = 0; i < n - len  + 1 ; i++) {
-                int j = i + len - 1;
+    int solve(string& s, int start, int last) {
 
-                if (s[i] == s[j]) {
-                    dp[i][j] = dp[i + 1][j - 1];
-                } else {
-                    dp[i][j] = 1 + min(dp[i + 1][j], dp[i][j - 1]);
-                }
-            }
+        if (start >= last) {
+            return 0;
         }
 
-        return dp[0][n - 1];
+        if (dp[start][last] != -1) {
+            return dp[start][last];
+        }
+
+        if (s[start] == s[last]) {
+            return dp[start][last] =
+                solve(s, start + 1, last - 1);
+        }
+
+        int count = 1 + min(
+            solve(s, start + 1, last),
+            solve(s, start, last - 1)
+        );
+
+        return dp[start][last] = count;
+    }
+
+    int minInsertions(string s) {
+        memset(dp, -1, sizeof(dp));
+
+        return solve(s, 0, s.length() - 1);
     }
 };
