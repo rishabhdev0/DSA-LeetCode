bfada843d67eaa2641979cccb445c3ae6baaa7ad
Rishabh pandey <rishabh52003@gmail.com>
2026-08-24 23:06:17 +0530

    Time: 130 ms (79.18%) | Memory: 104.4 MB (77.97%) - LeetSync

--- 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.cpp
+++ 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.cpp
@@ -1,49 +1,38 @@
-// class Solution {
-// public:
-//     long long dp[100001][2];
-
-//     long long solve(vector<int>& nums, int idx, bool even) {
-//         if (idx == nums.size()) return 0;
-
-//         if (dp[idx][even] != -1)
-//             return dp[idx][even];
-
-//         // option 1: skip current element
-//         long long skip = solve(nums, idx + 1, even);
-
-//         // option 2: take current element
-//         long long take;
-//         if (even)
-//             take = nums[idx] + solve(nums, idx + 1, !even);
-//         else
-//             take = -nums[idx] + solve(nums, idx + 1, !even);
-
-//         return dp[idx][even] = max(take, skip);
-//     }
-
-//     long long maxAlternatingSum(vector<int>& nums) {
-//         memset(dp, -1, sizeof(dp));
-//         return solve(nums, 0, true); // start with even
-//     }
-// };
-
-// bottom - up -> tabulation
-
 class Solution {
 public:
-    long long maxAlternatingSum(vector<int>& nums) {
-        int n = nums.size();
-        vector<vector<long long>> dp(n + 1, vector<long long>(2, 0));
+    long long dp[100001][2];
+    int n;
 
-        for (int i = 1; i <= n; i++) {
-            dp[i][0] = max(dp[i-1][0], dp[i-1][1] + nums[i-1]); // even
-            dp[i][1] = max(dp[i-1][1], dp[i-1][0] - nums[i-1]); // odd
+    long long solve(vector<int>& nums, int idx, int sign) {
+        if (idx >= n)
+            return 0;
+
+        if (dp[idx][sign] != -1)
+            return dp[idx][sign];
+
+        // Skip nums[idx]
+        long long notTake = solve(nums, idx + 1, sign);
+
+        // Take nums[idx]
+        long long take;
+
+        if (sign == 0) {
+            // + nums[idx]
+            take = nums[idx] + solve(nums, idx + 1, 1);
+        }
+        else {
+            // - nums[idx]
+            take = -nums[idx] + solve(nums, idx + 1, 0);
         }
 
-        return dp[n][0];
+        return dp[idx][sign] = max(take, notTake);
+    }
+
+    long long maxAlternatingSum(vector<int>& nums) {
+        n = nums.size();
+
+        memset(dp, -1, sizeof(dp));
+
+        return solve(nums, 0, 0);
     }
 };
-
-
-auto init = atexit([]() { ofstream("display_runtime.txt") << "000"; });
-
