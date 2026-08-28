ff06defdbf2db1d2d8778e3fddcb499bde6ea0a8
Rishabh pandey <rishabh52003@gmail.com>
2026-08-28 18:30:34 +0530

    Time: 101 ms (41.87%) | Memory: 177.5 MB (45.02%) - LeetSync

--- 2403-count-unreachable-pairs-of-nodes-in-an-undirected-graph/count-unreachable-pairs-of-nodes-in-an-undirected-graph.cpp
+++ 2403-count-unreachable-pairs-of-nodes-in-an-undirected-graph/count-unreachable-pairs-of-nodes-in-an-undirected-graph.cpp
@@ -1,39 +1,45 @@
 class Solution {
 public:
-    void dfs(int node, vector<bool>& visited, vector<vector<int>>& adj, int& size) {
+    int dfs(vector<vector<int>>& adj, int node, vector<bool>& visited) {
         visited[node] = true;
-        size++;
-        for(auto neighbor : adj[node]) {
-            if(!visited[neighbor]) {
-                dfs(neighbor, visited, adj, size);
+
+        int count = 1;
+
+        for (auto& neighbor : adj[node]) {
+            if (!visited[neighbor]) {
+                count += dfs(adj, neighbor, visited);
             }
         }
+
+        return count;
     }
-    
+
     long long countPairs(int n, vector<vector<int>>& edges) {
         vector<vector<int>> adj(n);
-        for(auto& edge : edges) {
+
+        for (auto& edge : edges) {
             int u = edge[0];
             int v = edge[1];
+
             adj[u].push_back(v);
             adj[v].push_back(u);
         }
-        
+
         vector<bool> visited(n, false);
-        long long result = 0;
-        int remainingNodes = n;
-        
-        for(int i = 0; i < n; i++) {
-            if(!visited[i]) {
-                int size = 0;
-                dfs(i, visited, adj, size);
-                result += (long long)size * (remainingNodes - size);
-                remainingNodes -= size;
+
+        long long ans = 0;
+        long long remaining = n;
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                int cnt = dfs(adj, i, visited);
+
+                ans += (long long)cnt * (remaining - cnt);
+
+                remaining -= cnt;
             }
         }
-        
-        return result;
+
+        return ans;
     }
 };
-
-auto init = atexit([]() { ofstream("display_runtime.txt") << "0"; });
