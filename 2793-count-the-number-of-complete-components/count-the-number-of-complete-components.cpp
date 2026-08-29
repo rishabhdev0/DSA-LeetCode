06237add49b1f73222d174cdef058e5a44cdb8cd
Rishabh pandey <rishabh52003@gmail.com>
2026-08-29 09:24:04 +0530

    Time: 37 ms (67.48%) | Memory: 128 MB (69.24%) - LeetSync

--- 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.cpp
+++ 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.cpp
@@ -1,47 +1,38 @@
 class Solution {
 public:
-    void dfs(
-        int node,
-        vector<vector<int>>& adj,int& vertexCount , int& degreeSum , vector<bool>& visited){
+    void dfs(vector<vector<int>>& adj , int node , int& edgeCount , vector<bool>& visited , int& nodeCount){
         visited[node] = true;
+        edgeCount++;
 
-        vertexCount++;
-        degreeSum += adj[node].size();
+        nodeCount += adj[node].size();
 
-        for (int neigh : adj[node]) {
-            if (!visited[neigh]) {
-                dfs(neigh, adj, vertexCount, degreeSum, visited);
+        for(auto neighbor : adj[node]){
+            if(!visited[neighbor]){
+                dfs(adj , neighbor , edgeCount , visited , nodeCount);
             }
         }
+
     }
-
     int countCompleteComponents(int n, vector<vector<int>>& edges) {
-        vector<vector<int>> adj(n);
-
-        for (auto& edge : edges) {
+        vector<vector<int>>adj(n);
+        for(auto& edge : edges){
             int u = edge[0];
             int v = edge[1];
-
             adj[u].push_back(v);
             adj[v].push_back(u);
         }
-
-        vector<bool> visited(n, false);
-        int result = 0;
-
-        for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                int vertexCount = 0;
-                int degreeSum = 0;
-
-                dfs(i, adj, vertexCount, degreeSum, visited);
-
-                if (degreeSum == vertexCount * (vertexCount - 1)) {
-                    result++;
+        int count = 0;
+        vector<bool>visited(n , false);
+        for(int i = 0 ; i < n ; i++){
+            if(!visited[i]){
+                int edgeCount = 0;
+                int nodeCount = 0;
+                dfs(adj , i , edgeCount , visited , nodeCount);
+                if(nodeCount == edgeCount * (edgeCount - 1)){
+                    count++;
                 }
             }
         }
-
-        return result;
+        return count;
     }
 };
