6cb73bb5491ed7636ed1525dfdac8fe23623aa4d
Rishabh pandey <rishabh52003@gmail.com>
2026-09-07 19:39:49 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (9.04%) - LeetSync

---
+++ 977-distinct-subsequences-ii/distinct-subsequences-ii.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int mod = 1e9 + 7;
+    int dp[2001];
+    vector<int>prev;
+
+    int solve(int n){
+        if(n == 0) return 1;
+
+        if(dp[n] != -1){
+            return dp[n];
+        }
+
+        int total = (2 * solve(n-1)) % mod;
+
+        if(prev[n] != 0){
+            int duplicate = solve(prev[n] - 1);
+            total = (total - duplicate + mod) % mod;
+        }
+
+        return dp[n] = total;
+
+    }
+    int distinctSubseqII(string s) {
+        int n = s.length();
+        memset(dp , -1 , sizeof(dp));
+        prev.assign(n+1 , 0);
+        vector<int>lastseen(26 , 0);
+        for(int i = 1 ; i<= n ; i++){
+            int idx = s[i-1] - 'a';
+            prev[i] = lastseen[idx];
+            lastseen[idx] = i;
+        }
+        return (solve(n) -  1 + mod) % mod;
+    }
+};
