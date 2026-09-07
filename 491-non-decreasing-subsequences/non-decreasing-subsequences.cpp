6194610a90d3386b4d5079a6a1847dbfebc62615
Rishabh pandey <rishabh52003@gmail.com>
2026-09-07 22:52:57 +0530

    Time: 32 ms (56.26%) | Memory: 29.9 MB (45.49%) - LeetSync

--- 491-non-decreasing-subsequences/non-decreasing-subsequences.cpp
+++ 491-non-decreasing-subsequences/non-decreasing-subsequences.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
-    vector<vector<int>> result;
-    vector<int> temp;
+    vector<vector<int>>result;
+    vector<int>temp;
 
-    void backtracking(vector<int>& nums, int start) {
-        if (temp.size() >= 2) {
+    void backtracking(vector<int>& nums , int start){
+        if(temp.size() >= 2){
             result.push_back(temp);
         }
 
-        unordered_set<int> used;
+        unordered_set<int>seen;
 
-        for (int i = start; i < nums.size(); i++) {
-            if (used.count(nums[i])) continue;
-
-            if (temp.empty() || nums[i] >= temp.back()) {
-                used.insert(nums[i]);
-
+        for(int i = start ; i < nums.size() ; i++){
+            if(seen.count(nums[i])) continue;
+            if(temp.empty() || nums[i] >= temp.back()){
+                seen.insert(nums[i]);
                 temp.push_back(nums[i]);
-                backtracking(nums, i + 1);
+                backtracking(nums , i + 1);
                 temp.pop_back();
             }
         }
     }
-
     vector<vector<int>> findSubsequences(vector<int>& nums) {
-        backtracking(nums, 0);
+        backtracking(nums , 0);
         return result;
     }
 };
