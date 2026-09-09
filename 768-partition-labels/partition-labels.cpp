33f28027141d44675ce2663d17a8ea0651c96949
Rishabh pandey <rishabh52003@gmail.com>
2026-09-09 11:04:03 +0530

    Time: 2 ms (32.69%) | Memory: 8.8 MB (86.08%) - LeetSync

--- 768-partition-labels/partition-labels.cpp
+++ 768-partition-labels/partition-labels.cpp
@@ -2,22 +2,29 @@ class Solution {
 public:
     vector<int> partitionLabels(string s) {
         int n = s.length();
-        vector<int>result;
-        vector<int> freq(26 , -1);
-       // last index
-        for(int i = 0 ; i<n;i++){
-           freq[s[i] - 'a'] = i;
+        vector<int>lastseen(26 , 0);
+
+        for(int  i= 0 ; i < n ; i++){
+            lastseen[s[i] - 'a'] = i;
         }
-       int i = 0;
-        while(i<n){
-            int last_idx = freq[s[i]-'a'];
-            int j = i;
-            while(j<last_idx){
-                last_idx = max(last_idx , freq[s[j]-'a']);
-                j++;
+
+        int j = 0;
+        vector<int>result;
+
+        while(j <  n){
+            int max_len = lastseen[s[j] - 'a'];
+
+            int i = j;
+
+            while(i < max_len){
+                max_len = max(max_len , lastseen[s[i] - 'a']);
+                i++;
             }
-            result.push_back(j-i+1);
-            i = j+1;
+
+            result.push_back(i - j + 1);
+
+            j = i + 1;
+           
         }
         return result;
     }
