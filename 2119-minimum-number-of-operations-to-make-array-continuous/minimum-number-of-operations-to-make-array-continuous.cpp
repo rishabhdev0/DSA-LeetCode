86496cad33beb185bbe93e0368ffc04c7bccd129
Rishabh pandey <rishabh52003@gmail.com>
2026-09-17 12:48:24 +0530

    Time: 249 ms (24.80%) | Memory: 110.9 MB (25.15%) - LeetSync

--- 2119-minimum-number-of-operations-to-make-array-continuous/minimum-number-of-operations-to-make-array-continuous.cpp
+++ 2119-minimum-number-of-operations-to-make-array-continuous/minimum-number-of-operations-to-make-array-continuous.cpp
@@ -2,31 +2,25 @@ class Solution {
 public:
     int minOperations(vector<int>& nums) {
         int n = nums.size();
-        sort(nums.begin(), nums.end());
 
-        vector<int> unique;
-        unique.push_back(nums[0]);
-        for(int i = 1; i < n; i++) {
-            if(nums[i] != nums[i-1]) {
-                unique.push_back(nums[i]);
-            }
+        set<int> seen(nums.begin(), nums.end());
+
+        vector<int> temp(seen.begin(), seen.end());
+
+        int result = n;
+
+        for (int i = 0; i < temp.size(); i++) {
+            int left = temp[i];
+            int right = left + n - 1;
+
+            int j = upper_bound(temp.begin(), temp.end(), right) - temp.begin();
+
+            int withinRange = j - i;
+            int outofRange = n - withinRange;
+
+            result = min(result, outofRange);
         }
 
-        int i = 0;
-        int j = 0;
-        int m = unique.size();
-        int max_len = 0;
-
-        while(j < m) {
-
-            while(unique[j] - unique[i] >= n) {
-                i++;
-            }
-
-            max_len = max(max_len, j - i + 1);
-            j++;
-        }
-
-        return n - max_len;
+        return result;
     }
 };
